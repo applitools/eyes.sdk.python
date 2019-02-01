d32570d03cdfa8446f142f1d52a058890d5508f4
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-02-01 16:00:46 +0200

    Add pre-commit managment

--- tasks.py
+++ tasks.py
@@ -6,16 +6,16 @@ here = path.dirname(path.abspath(__file__))
 
 
 @task
-def clean(c, docs=False, bytecode=False, dist=True, extra=''):
-    patterns = ['build']
+def clean(c, docs=False, bytecode=False, dist=True, extra=""):
+    patterns = ["build"]
     if docs:
-        patterns.append('docs/_build')
+        patterns.append("docs/_build")
     if bytecode:
-        patterns.append('**/*.pyc')
+        patterns.append("**/*.pyc")
     if dist:
-        patterns.append('**/dist')
-        patterns.append('**/build')
-        patterns.append('**/*.egg-info')
+        patterns.append("**/dist")
+        patterns.append("**/build")
+        patterns.append("**/*.egg-info")
     if extra:
         patterns.append(extra)
     for pattern in patterns:
@@ -23,40 +23,29 @@ def clean(c, docs=False, bytecode=False, dist=True, extra=''):
 
 
 @task(pre=[clean])
-def dist(c, core=None,
-         selenium=None, images=None, prod=False):
-    packages = list(_packages_resolver(core, selenium, images,
-                                       full_path=True, path_as_str=True))
-    dest = 'pypi' if prod else 'test'
+def dist(c, core=None, selenium=None, images=None, prod=False):
+    packages = list(
+        _packages_resolver(core, selenium, images, full_path=True, path_as_str=True)
+    )
+    dest = "pypi" if prod else "test"
     for pack_path in packages:
         with c.cd(pack_path):
             c.run("python setup.py build bdist_wheel", echo=True)
-            c.run('twine upload -r {dest} dist/*'.format(dest=dest), echo=True)
+            c.run("twine upload -r {dest} dist/*".format(dest=dest), echo=True)
 
 
 @task
 def install_requirements(c, dev=None, testing=None, lint=None):
-    dev_requires = [
-        'ipython',
-        'ipdb',
-        'bumpversion',
-        'wheel',
-        'twine',
-    ]
+    dev_requires = ["ipython", "ipdb", "bumpversion", "wheel", "twine", "pre-commit"]
     testing_requires = [
-        'pytest==3.9.3',
-        'pytest-cov',
-        'pytest-xdist',
-        'virtualenv',
-        'pytest-virtualenv',
-        'mock'
+        "pytest==3.9.3",
+        "pytest-cov",
+        "pytest-xdist",
+        "virtualenv",
+        "pytest-virtualenv",
+        "mock",
     ]
-    lint_requires = [
-        'flake8',
-        'flake8-import-order',
-        'flake8-bugbear',
-        'mypy',
-    ]
+    lint_requires = ["flake8", "flake8-import-order", "flake8-bugbear", "mypy"]
     if testing:
         requires = testing_requires
     elif dev:
@@ -65,13 +54,14 @@ def install_requirements(c, dev=None, testing=None, lint=None):
         requires = lint_requires
     else:
         requires = dev_requires + testing_requires + lint_requires
-    c.run("pip install {}".format(' '.join(requires)), echo=True)
+    c.run("pip install {}".format(" ".join(requires)), echo=True)
 
 
-def _packages_resolver(core=None, selenium=None, images=None,
-                       full_path=False, path_as_str=False):
+def _packages_resolver(
+    core=None, selenium=None, images=None, full_path=False, path_as_str=False
+):
     packages = []
-    core_pkg, selenium_pkg, images_pkg = 'eyes_core', 'eyes_selenium', 'eyes_images'
+    core_pkg, selenium_pkg, images_pkg = "eyes_core", "eyes_selenium", "eyes_images"
 
     if core:
         packages.append(core_pkg)
@@ -91,50 +81,51 @@ def _packages_resolver(core=None, selenium=None, images=None,
 
 
 @task
-def install_packages(c, core=None,
-                     selenium=None, images=None):
-    packages = _packages_resolver(core, selenium, images,
-                                  full_path=True, path_as_str=True)
+def install_packages(c, core=None, selenium=None, images=None):
+    packages = _packages_resolver(
+        core, selenium, images, full_path=True, path_as_str=True
+    )
     for pack in packages:
         c.run("pip install -U -e {}".format(pack), echo=True)
 
 
 @task
-def uninstall_packages(c, core=None,
-                       selenium=None, images=None):
+def uninstall_packages(c, core=None, selenium=None, images=None):
     packages = _packages_resolver(core, selenium, images)
-    c.run("pip uninstall {}".format(' '.join(packages)), echo=True)
+    c.run("pip uninstall {}".format(" ".join(packages)), echo=True)
 
 
 @task
-def pep_check(c, core=None,
-              selenium=None, images=None):
+def pep_check(c, core=None, selenium=None, images=None):
     for pack in _packages_resolver(core, selenium, images, full_path=True):
-        c.run('flake8 {}'.format(pack), echo=True)
+        c.run("flake8 {}".format(pack), echo=True)
 
 
 @task
-def mypy_check(c, core=None,
-               selenium=None, images=None):
+def mypy_check(c, core=None, selenium=None, images=None):
     for pack in _packages_resolver(core, selenium, images, full_path=True):
-        c.run('mypy --no-incremental --ignore-missing-imports {}/applitools'.format(pack), echo=True)
+        c.run(
+            "mypy --no-incremental --ignore-missing-imports {}/applitools".format(pack),
+            echo=True,
+        )
 
 
 @task
-def test_run_packs(c, core=None,
-                   selenium=None, images=None, appium=None):
+def test_run_packs(c, core=None, selenium=None, images=None, appium=None):
     if core:
         pass
     elif selenium:
-        c.run('pytest -n 2 --platform="Linux" --browser chrome --headless 1 tests/functional/eyes_selenium')
+        c.run(
+            'pytest -n 2 --platform="Linux" --browser chrome --headless 1 tests/functional/eyes_selenium'
+        )
     elif images:
-        c.run('pytest tests/functional/eyes_images')
+        c.run("pytest tests/functional/eyes_images")
     elif appium:
-        c.run('pytest -n 5 --remote 1 tests/functional/eyes_selenium/test_appium.py')
+        c.run("pytest -n 5 --remote 1 tests/functional/eyes_selenium/test_appium.py")
     # for pack in _packages_resolver(core, selenium, images):
     #     c.run('pytest tests/functional/{}'.format(pack), echo=True)
 
 
 @task
 def test_run_integration(c):
-    c.run('pytest tests/test_integration.py')
+    c.run("pytest tests/test_integration.py")
