7d619814ab442cdf571fe75af6a91df6a17c22fe
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-02-01 16:57:05 +0200

    eyes_images is blacked

--- eyes_images/setup.py
+++ eyes_images/setup.py
@@ -9,42 +9,38 @@ here = path.abspath(path.dirname(__file__))
 
 
 def read(filename):
-    return codecs.open(path.join(here, filename), 'r', 'utf-8').read()
+    return codecs.open(path.join(here, filename), "r", "utf-8").read()
 
 
 # preventing ModuleNotFoundError caused by importing lib before installing deps
 def get_version(package_name):
-    version_file = read('applitools/{}/__version__.py'.format(package_name))
+    version_file = read("applitools/{}/__version__.py".format(package_name))
     try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$",
-                             version_file, re.M)[0]
+        version = re.findall(r"^__version__ = '([^']+)'\r?$", version_file, re.M)[0]
     except IndexError:
-        raise RuntimeError('Unable to determine version.')
+        raise RuntimeError("Unable to determine version.")
 
     return version
 
 
-install_requires = [
-    'eyes-core=={}'.format(get_version('images')),
-    'Pillow>=5.0.0',
-]
+install_requires = ["eyes-core=={}".format(get_version("images")), "Pillow>=5.0.0"]
 # using this way of defining instead of 'typing>=3.5.2; python_version<="3.4"'
 # for run on old version of setuptools without issues
 if sys.version_info < (3, 5):
     # typing module was added as builtin in Python 3.5
-    install_requires.append('typing >= 3.5.2')
+    install_requires.append("typing >= 3.5.2")
 
 setup(
-    name='eyes_images',
-    version=get_version('images'),
+    name="eyes_images",
+    version=get_version("images"),
     packages=find_packages(),
-    url='http://www.applitools.com',
-    license='Apache License, Version 2.0',
-    author='Applitools Team',
-    author_email='team@applitools.com',
-    description='Applitools Python SDK. Images package',
-    long_description=read('README.rst'),
-    long_description_content_type='text/x-rst',
+    url="http://www.applitools.com",
+    license="Apache License, Version 2.0",
+    author="Applitools Team",
+    author_email="team@applitools.com",
+    description="Applitools Python SDK. Images package",
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
@@ -54,16 +50,14 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Topic :: Software Development :: Quality Assurance",
-        "Topic :: Software Development :: Testing"
+        "Topic :: Software Development :: Testing",
     ],
-    keywords='applitools eyes eyes_images',
+    keywords="applitools eyes eyes_images",
     install_requires=install_requires,
-    package_data={
-        '':     ['README.rst', 'LICENSE'],
-        'images': ['py.typed'],
-    },
+    package_data={"": ["README.rst", "LICENSE"], "images": ["py.typed"]},
     project_urls={
-        'Bug Reports': 'https://github.com/applitools/eyes.sdk.python/issues',
-        'Source':      'https://github.com/applitools/eyes.sdk.python/tree/master/eyes_images/applitools/images',
+        "Bug Reports": "https://github.com/applitools/eyes.sdk.python/issues",
+        "Source": "https://github.com/applitools/eyes.sdk.python/tree/master/"
+        "eyes_images/applitools/images",
     },
 )
