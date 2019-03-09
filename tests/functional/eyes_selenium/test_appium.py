4327a40e6ff3f9759c19c94fc0968a256c0f3d6a
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-03-09 16:32:45 +0200

    Update appium test

--- tests/functional/eyes_selenium/test_appium.py
+++ tests/functional/eyes_selenium/test_appium.py
@@ -1,15 +1,7 @@
 import pytest
 from appium import webdriver
-from selenium.webdriver.common.by import By
 
-from applitools.selenium import (
-    FloatingBounds,
-    FloatingRegion,
-    IgnoreRegionBySelector,
-    Region,
-    StitchMode,
-    Target,
-)
+from applitools.selenium import StitchMode
 
 
 @pytest.fixture
@@ -51,25 +43,17 @@ def test_ios_native(eyes, driver):
     eyes.close()
 
 
-@pytest.mark.platform("Android")
-@pytest.mark.test_page_url("http://applitools.com")
-@pytest.mark.eyes(force_full_page_screenshot=True, stitch_mode=StitchMode.CSS)
+@pytest.mark.platform("Android", "iOS")
+@pytest.mark.test_page_url("https://applitools.com/helloworld")
+@pytest.mark.parametrize(
+    "eyes",
+    [
+        {"force_full_page_screenshot": True, "stitch_mode": StitchMode.CSS},
+        {"force_full_page_screenshot": False, "stitch_mode": StitchMode.Scroll},
+    ],
+    indirect=True,
+    ids=lambda o: "with FSP" if o["force_full_page_screenshot"] else "no FSP",
+)
 def test_final_application(eyes_open):
     eyes, driver = eyes_open
-    eyes.check_window(
-        "Home",
-        target=(
-            Target()
-            .ignore(IgnoreRegionBySelector(By.CLASS_NAME, "hero-container"))
-            .floating(
-                FloatingRegion(Region(10, 20, 30, 40), FloatingBounds(10, 0, 20, 10))
-            )
-        ),
-    )
-
-    hero = driver.find_element_by_class_name("hero-container")
-    eyes.check_region_by_element(
-        hero,
-        "Page Hero",
-        target=(Target().ignore(Region(20, 20, 50, 50), Region(40, 40, 10, 20))),
-    )
+    eyes.check_window("Home")
