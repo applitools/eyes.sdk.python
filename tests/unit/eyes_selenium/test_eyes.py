4724177a4040e4dca6715ca1ff8ddc37d50f549c
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-05-15 13:23:38 +0300

    Add test_match_level

--- tests/unit/eyes_selenium/test_eyes.py
+++ tests/unit/eyes_selenium/test_eyes.py
@@ -1,18 +1,24 @@
 import pytest
+from applitools.common import MatchLevel
 
 from applitools.core import NullScaleProvider
 from applitools.selenium import Eyes
 from applitools.selenium.visual_grid import VisualGridRunner
 
 
-@pytest.fixture(scope="function")
-def eyes():
-    return Eyes()
+def pytest_generate_tests(metafunc):
+    if "eyes" in metafunc.fixturenames:
+        metafunc.parametrize("eyes", ["selenium", "visual_grid"], indirect=True)
 
 
 @pytest.fixture(scope="function")
-def vg_eyes():
-    return Eyes(VisualGridRunner())
+def eyes(request):
+    if request.param == "selenium":
+        return Eyes()
+    elif request.param == "visual_grid":
+        return Eyes(VisualGridRunner())
+    else:
+        raise ValueError("invalid internal test config")
 
 
 def test_set_get_scale_ratio(eyes):
@@ -23,6 +29,9 @@ def test_set_get_scale_ratio(eyes):
     assert eyes.scale_ratio == NullScaleProvider.UNKNOWN_SCALE_RATIO
 
 
-def test_initialization():
-    eyes = Eyes()
-    eyes
+def test_match_level(eyes):
+    assert eyes.match_level == MatchLevel.STRICT
+    eyes.match_level = MatchLevel.EXACT
+    assert eyes.match_level == MatchLevel.EXACT
+    eyes.match_level = MatchLevel.LAYOUT
+    assert eyes.match_level == MatchLevel.LAYOUT
