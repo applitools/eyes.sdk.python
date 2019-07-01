36618beeb2f0c1cd4e65621ae7e0c59a0080f805
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-07-01 16:41:05 +0300

    Add cut providers to core, common and selenium import namespaces

--- eyes_images/applitools/images/__init__.py
+++ eyes_images/applitools/images/__init__.py
@@ -1,6 +1,21 @@
 from applitools.common import BatchInfo, MatchLevel, Region, logger
+from applitools.core.cut import (
+    FixedCutProvider,
+    UnscaledFixedCutProvider,
+    NullCutProvider,
+)
 from applitools.images.fluent import Target
 
 from .eyes import Eyes
 
-__all__ = ("Eyes", "BatchInfo", "Region", "MatchLevel", "logger", "Target")
+__all__ = (
+    "Eyes",
+    "BatchInfo",
+    "Region",
+    "MatchLevel",
+    "logger",
+    "Target",
+    "FixedCutProvider",
+    "UnscaledFixedCutProvider",
+    "NullCutProvider",
+)
