df6e62d41b3f8ff499230e02cd88371b4079d8ff
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-07-09 17:43:29 +0300

    Add test_check_settings

---
+++ tests/unit/eyes_core/test_check_settings.py
@@ -0,0 +1,21 @@
+from applitools.core import CheckSettings
+
+
+def test_set_get_use_dom():
+    cs = CheckSettings().use_dom(True)
+    assert cs.values.use_dom
+
+
+def test_set_get_send_dom():
+    cs = CheckSettings().send_dom(True)
+    assert cs.values.send_dom
+
+
+def test_set_get_enable_patterns():
+    cs = CheckSettings().enable_patterns(True)
+    assert cs.values.enable_patterns
+
+
+def test_set_get_ignore_displacement():
+    cs = CheckSettings().ignore_displacement(True)
+    assert cs.values.ignore_displacement
