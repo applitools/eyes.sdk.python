16e86e878bc15c8f166f808156389ca3e3aa7680
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-07-19 17:07:13 +0300

    Change func name

--- tests/unit/eyes_selenium/test_selenium_check_settings.py
+++ tests/unit/eyes_selenium/test_selenium_check_settings.py
@@ -8,7 +8,7 @@ from applitools.selenium import Region, EyesWebElement
 from applitools.selenium.fluent import SeleniumCheckSettings
 
 
-def get_cs_for_method(method_name, *args):
+def get_cs_from_method(method_name, *args):
     """
     Return initialized CheckSettings instance and invoked `method_name` with `args`
 
@@ -25,62 +25,62 @@ def get_regions_from_(method_name, *args):
 
     :param method_name: layout, ignore, strict or content
     """
-    cs = get_cs_for_method(method_name, *args)
+    cs = get_cs_from_method(method_name, *args)
     regions = getattr(cs.values, "{}_regions".format(method_name))
     return regions
 
 
 def test_check_frame(method_name="frame"):
     frame_reference = "frame-name-or-id"
-    cs = get_cs_for_method(method_name, frame_reference)
+    cs = get_cs_from_method(method_name, frame_reference)
     assert cs.values.frame_chain[0].frame_name_or_id == frame_reference
 
     frame_index = 3
-    cs = get_cs_for_method(method_name, frame_index)
+    cs = get_cs_from_method(method_name, frame_index)
     assert cs.values.frame_chain[0].frame_index == frame_index
 
     frame_element = MagicMock(EyesWebElement)
-    cs = get_cs_for_method(method_name, frame_element)
+    cs = get_cs_from_method(method_name, frame_element)
     assert cs.values.frame_chain[0].frame_element == frame_element
 
 
 def test_check_region_with_region(method_name="region"):
     region = Region(0, 1, 2, 3)
-    cs = get_cs_for_method(method_name, region)
+    cs = get_cs_from_method(method_name, region)
     assert cs.values.target_region == region
 
     selector_or_xpath = ".cssSelector_or_XPATH"
-    cs = get_cs_for_method(method_name, selector_or_xpath)
+    cs = get_cs_from_method(method_name, selector_or_xpath)
     assert cs.values.target_selector == selector_or_xpath
 
 
 def test_check_region_with_elements(method_name="region"):
     eyes_element = MagicMock(EyesWebElement)
-    cs = get_cs_for_method(method_name, eyes_element)
+    cs = get_cs_from_method(method_name, eyes_element)
     assert cs.values.target_element == eyes_element
 
     selenium_element = MagicMock(SeleniumWebElement)
-    cs = get_cs_for_method(method_name, selenium_element)
+    cs = get_cs_from_method(method_name, selenium_element)
     assert cs.values.target_element == selenium_element
 
     appium_element = MagicMock(AppiumWebElement)
-    cs = get_cs_for_method(method_name, appium_element)
+    cs = get_cs_from_method(method_name, appium_element)
     assert cs.values.target_element == appium_element
 
 
 def test_check_region_with_by_params(method_name="region"):
     selector_or_xpath = ".cssSelector_or_XPATH"
-    cs = get_cs_for_method(method_name, [By.NAME, "some-name"])
+    cs = get_cs_from_method(method_name, [By.NAME, "some-name"])
     assert cs.values.target_selector == '[name="some-name"]'
-    cs = get_cs_for_method(method_name, [By.ID, "ident"])
+    cs = get_cs_from_method(method_name, [By.ID, "ident"])
     assert cs.values.target_selector == "#ident"
-    cs = get_cs_for_method(method_name, [By.CLASS_NAME, "class_name"])
+    cs = get_cs_from_method(method_name, [By.CLASS_NAME, "class_name"])
     assert cs.values.target_selector == ".class_name"
-    cs = get_cs_for_method(method_name, [By.TAG_NAME, "tag_name"])
+    cs = get_cs_from_method(method_name, [By.TAG_NAME, "tag_name"])
     assert cs.values.target_selector == "tag_name"
-    cs = get_cs_for_method(method_name, [By.CSS_SELECTOR, selector_or_xpath])
+    cs = get_cs_from_method(method_name, [By.CSS_SELECTOR, selector_or_xpath])
     assert cs.values.target_selector == selector_or_xpath
-    cs = get_cs_for_method(method_name, [By.XPATH, selector_or_xpath])
+    cs = get_cs_from_method(method_name, [By.XPATH, selector_or_xpath])
     assert cs.values.target_selector == selector_or_xpath
 
 
