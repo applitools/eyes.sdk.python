f212ca12a9aaa50e70e5c0b5a80ed750291ea9c7
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-08-02 14:32:08 +0300

    feature: Make DictAccessMixin iterable

--- eyes_common/applitools/common/geometry.py
+++ eyes_common/applitools/common/geometry.py
@@ -20,6 +20,8 @@ __all__ = ("Point", "Region", "CoordinatesType", "RectangleSize")
 
 class DictAccessMixin(object):
     def __getitem__(self, item):
+        if isinstance(item, int):
+            item = self.__slots__[item]
         if item not in self.__slots__:
             raise KeyError
         return getattr(self, item)
