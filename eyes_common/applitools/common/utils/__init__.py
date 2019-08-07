41eb9c1d863aa738ca07ff793966afaad07c3259
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-08-07 18:23:09 +0300

    feat: add to_rfc1123_datetime helper function; create datetime_utils

--- eyes_common/applitools/common/utils/__init__.py
+++ eyes_common/applitools/common/utils/__init__.py
@@ -11,6 +11,11 @@ from .compat import (  # type: ignore # noqa
     urlsplit,
     urlunsplit,
 )
+from .datetime_utils import (  # type: ignore # noqa
+    UTC,
+    current_time_in_rfc1123,
+    to_rfc1123_datetime,
+)
 from .general_utils import cached_property  # noqa
 
 __all__ = compat.__all__ + ("image_utils", "argument_guard")  # noqa
