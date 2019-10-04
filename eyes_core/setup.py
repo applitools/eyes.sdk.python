bf4b4dc9a82588b406f906fc1e89bbc3d055f46f
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-10-04 19:34:10 +0300

    chore: update version of dependencies

--- eyes_core/setup.py
+++ eyes_core/setup.py
@@ -25,9 +25,9 @@ def get_version(package_name):
 
 install_requires = [
     "eyes-common=={}".format(get_version("core")),
-    "attrs >= 18,<19",
-    "Pillow >= 5.0.0,<7.0.0",
-    "requests>=2.1.0,<2.21.0",
+    "attrs>=18,<=19.2",
+    "Pillow>=5.0.0,<7.0.0",  # 7 and above doesnt support python 2
+    "requests>=2.1.0,<2.22.0",
 ]
 # using this way of defining instead of 'typing>=3.5.2; python_version<="3.4"'
 # for run on old version of setuptools without issues
