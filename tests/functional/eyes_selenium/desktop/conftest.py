912af863af9eebca4ae3c20332fa66b19145cf15
Serhii Khalymon <sergiykhalimon@gmail.com>
2019-12-02 12:03:11 +0200

    test: update Batch naming for desktop vg

--- tests/functional/eyes_selenium/desktop/conftest.py
+++ tests/functional/eyes_selenium/desktop/conftest.py
@@ -16,13 +16,7 @@ def vg_eyes():
     vgr = VisualGridRunner(1)
     eyes = Eyes(vgr)
     eyes.configuration.add_browser(700, 460, BrowserType.CHROME)
-    eyes.configuration.batch = BatchInfo("Python SDK Desktop VG")
     return eyes
-
-
-@pytest.fixture
-def batch_info():
-    return BatchInfo("Python SDK Desktop Sel")
 
 
 def pytest_generate_tests(metafunc):
@@ -33,6 +27,12 @@ def pytest_generate_tests(metafunc):
         stitches = [dict(stitch_mode=StitchMode.CSS)]
         if "eyes_class" in metafunc.fixturenames:
             metafunc.parametrize("eyes_class", [lambda: vg_eyes()]),
+
+    if "batch_info" in metafunc.fixturenames:
+        b_info = BatchInfo("Python SDK Desktop Sel")
+        if strtobool(os.getenv("TEST_RUN_ON_VG", "False")):
+            b_info = BatchInfo("Python SDK Desktop VG")
+        metafunc.parametrize("batch_info", [b_info])
 
     if "page" in metafunc.fixturenames:
         metafunc.parametrize("page", ["mobile", "desktop", "scrolled_mobile"])
