450c3cd74e5e926198f07688ade8dafcbc198cda
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-01-17 04:57:11 +0200

    rename prepare_result_data_for_makereport to prepare_result_data

--- tests/utils.py
+++ tests/utils.py
@@ -29,7 +29,7 @@ REPORT_DATA = {
 }
 
 
-def prepare_result_data_for_makereport(test_name, passed, parameters):
+def prepare_result_data(test_name, passed, parameters):
     test_name = underscore_to_camelcase(test_name)
     result = dict(test_name=test_name, passed=passed)
     if parameters:
@@ -55,9 +55,7 @@ def prepare_result_data_for_makereport(test_name, passed, parameters):
 
 def send_result_report(test_name, passed, parameters=None, group="selenium"):
     report_data = copy(REPORT_DATA)
-    report_data["results"] = prepare_result_data_for_makereport(
-        test_name, passed, parameters
-    )
+    report_data["results"] = prepare_result_data(test_name, passed, parameters)
     report_data["group"] = group
     r = requests.post(urljoin(REPORT_BASE_URL, "/result"), data=json.dumps(report_data))
     print("Result report send: {} - {}".format(r.status_code, r.text))
