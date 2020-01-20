d55dac09ae2309fa85e0432c781d396af7c2da9d
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-01-20 17:52:25 +0200

    rfe_trello_1479_add_browser_version (#92)
    * feat: add one and two version back browsers version
    
    * test: add relevant browsers

--- eyes_common/applitools/common/selenium/misc.py
+++ eyes_common/applitools/common/selenium/misc.py
@@ -2,8 +2,15 @@ from enum import Enum
 
 
 class BrowserType(Enum):
-    CHROME = "chrome"
-    FIREFOX = "firefox"
+    CHROME = "chrome-0"
+    CHROME_ONE_VERSION_BACK = "chrome-1"
+    CHROME_TWO_VERSION_BACK = "chrome-2"
+    FIREFOX = "firefox-0"
+    FIREFOX_ONE_VERSION_BACK = "firefox-1"
+    FIREFOX_TWO_VERSION_BACK = "firefox-2"
+    SAFARI = "safari-0"
+    SAFARI_ONE_VERSION_BACK = "safari-1"
+    SAFARI_TWO_VERSION_BACK = "safari-2"
     IE_10 = "ie10"
     IE_11 = "ie"
     EDGE = "edge"
