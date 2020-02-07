76c495f8fbe2c2b7733b5fb51f4613d91368ade4
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-02-07 02:07:49 +0200

    BatchClose 1380 (#105)
    * feat: add BatchClose
    * add notify_on_completion to BatchInfo
    * refactor: use str2bool to check for boolean in ENV
    
    * test: add unit test_batch_close.py
    * test: add APPLITOOLS_BATCH_NOTIFY to test

--- eyes_common/applitools/common/config.py
+++ eyes_common/applitools/common/config.py
@@ -9,6 +9,7 @@ from applitools.common.geometry import RectangleSize
 from applitools.common.match import ImageMatchSettings, MatchLevel
 from applitools.common.server import FailureReports, SessionType
 from applitools.common.utils import UTC, argument_guard
+from applitools.common.utils.converters import str2bool
 from applitools.common.utils.general_utils import get_env_with_prefix
 from applitools.common.utils.json_utils import JsonInclude
 
@@ -38,6 +39,7 @@ class BatchInfo(object):
         metadata={JsonInclude.NAME: "batchSequenceName"}
     )  # type: Optional[Text]
     id = attr.ib(converter=str, metadata={JsonInclude.THIS: True})  # type: Text
+    notify_on_completion = attr.ib(metadata={JsonInclude.NON_NONE: True})
 
     def __init__(self, name=None, started_at=None, batch_sequence_name=None):
         # type: (Optional[Text], Optional[datetime], Optional[Text]) -> None
@@ -47,6 +49,9 @@ class BatchInfo(object):
         self.name = get_env_with_prefix("APPLITOOLS_BATCH_NAME")
         self.started_at = datetime.now(UTC)
         self.sequence_name = get_env_with_prefix("APPLITOOLS_BATCH_SEQUENCE")
+        self.notify_on_completion = str2bool(
+            get_env_with_prefix("APPLITOOLS_BATCH_NOTIFY")
+        )
 
         if name:
             self.name = name
---
+++ tests/functional/eyes_selenium/selenium/test_batch_close.py
@@ -0,0 +1,17 @@
+import pytest
+
+from applitools.core import BatchClose
+
+
+@pytest.fixture(autouse=True)
+def batch_close_enabled(batch_info):
+    batch_enabled = BatchClose().set_batch_ids(batch_info.id)
+    yield
+    batch_enabled.close()
+
+
+def test_batch_close(eyes, driver):
+    driver.get("https://demo.applitools.com")
+    eyes.open(driver, "TestBatchNotificationApp", "TestBatchClose")
+    eyes.check_window()
+    eyes.close(False)
