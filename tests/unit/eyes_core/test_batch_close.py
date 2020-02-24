fa8edd4c399a02cb8c86dfa0105b4d78200f0cd8
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-02-24 18:04:07 +0200

    test: update test_pass_multiple_batches_ids to respect batch id encoding

--- tests/unit/eyes_core/test_batch_close.py
+++ tests/unit/eyes_core/test_batch_close.py
@@ -27,9 +27,9 @@ def test_batch_no_api_key_error(env_dont_close):
 @patch.dict(os.environ, {"APPLITOOLS_API_KEY": "Some-key"})
 def test_pass_multiple_batches_ids():
     with patch("requests.delete") as mocked_request:
-        BatchClose().set_batch_ids("test-batch-id").close()
-        assert "test-batch-id" in mocked_request.call_args[0][0]
-        BatchClose().set_batch_ids("test-batch-id", "test-batch-second").close()
-        assert "test-batch-second" in mocked_request.call_args[0][0]
+        BatchClose().set_batch_ids("test batch-id").close()
+        assert "test+batch-id" in mocked_request.call_args[0][0]
+        BatchClose().set_batch_ids("test-batch-id", "test-batch//@second").close()
+        assert "test-batch%2F%2F%40second" in mocked_request.call_args[0][0]
         BatchClose().set_batch_ids(["test-batch-id", "test-batch-second"]).close()
         assert "test-batch-second" in mocked_request.call_args[0][0]
