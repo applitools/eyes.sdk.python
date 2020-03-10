fe5249163403da330de30b8f4538594ec46fa62e
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-03-10 18:29:28 +0200

    chore: move mobile_eyes fixture to test_mobile.py

--- tests/functional/eyes_selenium/mobile/conftest.py
+++ tests/functional/eyes_selenium/mobile/conftest.py
@@ -1,11 +1,6 @@
-import os
-from copy import copy
-
 import pytest
-from appium import webdriver as appium_webdriver
-from selenium.common.exceptions import WebDriverException
 
-from applitools.common import BatchInfo, StitchMode, logger
+from applitools.common import BatchInfo
 
 
 def pytest_generate_tests(metafunc):
@@ -16,68 +11,3 @@ def pytest_generate_tests(metafunc):
 @pytest.fixture
 def batch_info():
     return BatchInfo("Python SDK Mobile")
-
-
-@pytest.yield_fixture(scope="function")
-def mobile_eyes(request, page, eyes):
-    # configure eyes through @pytest.mark.parametrize('mobile_eyes', [], indirect=True)
-    browser_config = copy(getattr(request, "param", {}))
-    fully = browser_config.pop("fully", None)
-
-    test_name = "{name} {plat_ver} {dev_or} {page}".format(
-        name=browser_config["deviceName"],
-        plat_ver=browser_config["platformVersion"],
-        dev_or=browser_config["deviceOrientation"],
-        page=page,
-    )
-    test_suite_name = request.node.get_closest_marker("test_suite_name")
-    test_suite_name = (
-        test_suite_name.args[-1]
-        if test_suite_name
-        else "Eyes Selenium SDK - iOS Safari Cropping"
-    )
-
-    if fully:
-        test_name += " fully"
-
-    selenium_url = "https://{username}:{password}@ondemand.saucelabs.com:443/wd/hub".format(
-        username=os.getenv("SAUCE_USERNAME", None),
-        password=os.getenv("SAUCE_ACCESS_KEY", None),
-    )
-    desired_caps = browser_config.copy()
-    desired_caps["build"] = os.getenv("BUILD_TAG", None)
-    desired_caps["tunnelIdentifier"] = os.getenv("TUNNEL_IDENTIFIER", None)
-    desired_caps["name"] = "{} ({})".format(test_name, eyes.full_agent_id)
-
-    driver = appium_webdriver.Remote(
-        command_executor=selenium_url, desired_capabilities=desired_caps
-    )
-
-    eyes.stitch_mode = StitchMode.CSS
-    eyes.add_property("Orientation", browser_config["deviceOrientation"])
-    eyes.add_property("Stitched", "True" if fully else "False")
-
-    if driver is None:
-        raise WebDriverException("Never created!")
-
-    driver.get(
-        "https://applitools.github.io/demo/TestPages/DynamicResolution/{}.html".format(
-            page
-        )
-    )
-    driver = eyes.open(driver, test_suite_name, test_name)
-    yield eyes, fully
-
-    # report results
-    try:
-        driver.execute_script(
-            "sauce:job-result=%s" % str(not request.node.rep_call.failed).lower()
-        )
-    except WebDriverException:
-        # we can ignore the exceptions of WebDriverException type -> We're done with tests.
-        logger.info(
-            "Warning: The driver failed to quit properly. Check test and server side logs."
-        )
-    finally:
-        driver.quit()
-        eyes.close()
