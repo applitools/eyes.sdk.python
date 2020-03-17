9d2788eba1416d93411c729c4071351e0bf6afa1
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-03-17 19:57:03 +0200

    test: update test_check_element_and_window_in_sequence

--- tests/functional/eyes_selenium/visual_grid/test_additional.py
+++ tests/functional/eyes_selenium/visual_grid/test_additional.py
@@ -7,19 +7,25 @@ from applitools.selenium import (
 
 
 def test_check_element_and_window_in_sequence(driver, eyes, batch_info, vg_runner):
-    driver.get(
-        "https://applitools.github.io/demo/TestPages/VisualGridTestPage/index.html"
-    )
+    driver.get("https://demo.applitools.com")
     eyes = Eyes(vg_runner)
     eyes.set_configuration(
         Configuration(
             test_name="TestCheckElementAndWindow",
             app_name="Visual Grid Additional Test",
             batch=batch_info,
-        ).add_browser(1200, 800, BrowserType.CHROME)
+        ).add_browser(1024, 768, BrowserType.CHROME)
     )
     eyes.open(driver)
-    eyes.check("check element", Target.region("#scroll1").layout("#scroll1"))
-    eyes.check("check window", Target.window().layout("#scroll1"))
+    eyes.check_window("Step 1")
+    eyes.check(
+        "Step 2",
+        Target.region("body > div > div").layout(
+            "body > div > div > form > div:nth-child(1)"
+        ),
+    )
+    eyes.check(
+        "Step 3", Target.window().layout("body > div > div > form > div:nth-child(1)"),
+    )
     eyes.close_async()
-    vg_runner.get_all_test_results()
+    res = vg_runner.get_all_test_results()
