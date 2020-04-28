be9ee93b1e98de5b9e674dd302a6903cf9d3777c
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-04-28 14:54:44 +0300

    feat: add deploy event

---
+++ sendMail.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"version\":\"$2\", \"changeLog\":\"$3\", \"testCoverageGap\":\"$4\"}"
+  
