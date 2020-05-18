823814543e4e6fa7d5e9d6834f21bbc209627a6c
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-05-18 11:05:37 +0300

    test: add test of accessibility_validation in Configuration

--- tests/unit/eyes_core/test_config.py
+++ tests/unit/eyes_core/test_config.py
@@ -1,6 +1,11 @@
 import pytest
 
 from applitools.common import Configuration, MatchLevel
+from applitools.common.accessibility import (
+    AccessibilitySettings,
+    AccessibilityLevel,
+    AccessibilityGuidelinesVersion,
+)
 
 
 @pytest.fixture
@@ -28,3 +33,11 @@ def test_set_get_values_present_in_image_match_settings(config):
     config.ignore_displacements = True
     assert config.ignore_displacements
     assert config.default_match_settings.ignore_displacements
+
+    a_setting = AccessibilitySettings(
+        AccessibilityLevel.AA, AccessibilityGuidelinesVersion.WCAG_2_1
+    )
+    assert config.accessibility_validation is None
+    config.accessibility_validation = a_setting
+    assert config.accessibility_validation == a_setting
+    assert config.default_match_settings.accessibility_settings == a_setting
