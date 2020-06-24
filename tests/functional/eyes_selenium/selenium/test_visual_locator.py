d962131dd9649a2e61b407843cd215662d3d33ab
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-06-24 14:51:11 +0300

    test: run test_visual_locator with VG and CR

--- tests/functional/eyes_selenium/selenium/test_visual_locator.py
+++ tests/functional/eyes_selenium/selenium/test_visual_locator.py
@@ -1,12 +1,18 @@
+import pytest
+
 from applitools.common import Region
 from applitools.core import VisualLocator
-from applitools.selenium import Eyes, ClassicRunner
+from applitools.selenium import Eyes, ClassicRunner, VisualGridRunner
 
 
-def test_visual_locator(driver, eyes_class, eyes_runner):
+@pytest.mark.parametrize("eyes_runner", [ClassicRunner(), VisualGridRunner(1)])
+def test_visual_locator(driver, eyes_runner):
     driver.get("https://applitools.github.io/demo/TestPages/FramesTestPage/")
-    eyes = Eyes(ClassicRunner())
-    eyes.open(driver, "Applitools Eyes SDK", "testVisualLocators")
+    eyes = Eyes(eyes_runner)
+    test_name = "testVisualLocators"
+    if isinstance(eyes_runner, VisualGridRunner):
+        test_name += "_VG"
+    eyes.open(driver, "Applitools Eyes SDK", test_name)
 
     result = eyes.locate(VisualLocator.name("applitools_title"))
     eyes.close_async()
