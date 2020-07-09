aff97adb748cf0e766a91d86535b19d054825266
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-07-09 10:12:25 +0300

    docs: update deprecation msg

--- eyes_common/applitools/common/visual_grid.py
+++ eyes_common/applitools/common/visual_grid.py
@@ -1,4 +1,4 @@
 from . import logger
 from .ultrafastgrid import *  # noqa
 
-logger.deprecation("Use `applitools.common.ultrafastgrid` package instead")
+logger.deprecation("Import from `applitools.common` package instead")
