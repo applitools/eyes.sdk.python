641210b157f5757dc2f533271c0609e4c73b430c
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-07-15 17:19:03 +0300

    feat: allow to use Appium-Python-Client 1+ version with Python 3

--- eyes_selenium/setup.py
+++ eyes_selenium/setup.py
@@ -29,7 +29,6 @@ install_requires = [
     "tinycss2==0.6.1",  # last version that supports python 2
     "selenium>=2.53.0,<4",
     # uses for check if the mobile platform is used in the test
-    "Appium-Python-Client>=0.4,<1.0.0",
     "ua_parser==0.10.0",
     "attrs>=19.2.0,<20",
     "transitions>=0.6,<=0.8.3",
@@ -37,8 +36,10 @@ install_requires = [
 ]
 if sys.version_info <= (2, 7):
     install_requires.append("Pillow >= 5.0.0,<7.0.0")
+    install_requires.append("Appium-Python-Client>=0.4,<1.0.0")
 else:
-    install_requires.append("Pillow >= 7.0.0")
+    install_requires.append("Pillow >= 5.0.0")
+    install_requires.append("Appium-Python-Client>=0.4")
 # using this way of defining instead of 'typing>=3.5.2; python_version<="3.4"'
 # for run on old version of setuptools without issues
 if sys.version_info < (3, 5):
