e1205d3b4f3d5db51c1dcd37be59e1f276c709b2
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-07-15 21:56:43 +0300

    chore: set the upper bound of the dependency version

--- eyes_selenium/setup.py
+++ eyes_selenium/setup.py
@@ -40,9 +40,9 @@ if sys.version_info[:2] <= (2, 7):
     install_requires.append("Appium-Python-Client>=0.4,<1.0.0")
     install_requires.append("tinycss2==0.6.1")
 else:
-    install_requires.append("Pillow >= 5.0.0")
-    install_requires.append("Appium-Python-Client>=0.4")
-    install_requires.append("tinycss2>=0.6.1")
+    install_requires.append("Pillow >= 5.0.0,<8")
+    install_requires.append("Appium-Python-Client>=0.4,<2")
+    install_requires.append("tinycss2>=0.6.1,<2")
 
 # using this way of defining instead of 'typing>=3.5.2; python_version<="3.4"'
 # for run on old version of setuptools without issues
