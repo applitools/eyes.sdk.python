ea8a9fb44c4b2cedeb1f95db732a200fa63fff7d
Yurii <yurii.cherepovskiy@applitools.com>
2020-07-23 23:31:44 +0300

    Return back nvm install node

--- all_tests_report.sh
+++ all_tests_report.sh
@@ -1,1 +1,1 @@
-curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"specificRecipient\": \"$2\"}"
+curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"specificRecipient\": \"yurii.cherepovskiy@applitools.com\"}"
