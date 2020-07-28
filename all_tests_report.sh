ff7ce0d6bc54ad55b759f54114ef4bdec358c729
Yurii <yurii.cherepovskiy@applitools.com>
2020-07-28 11:44:19 +0300

    Remove test emails

--- all_tests_report.sh
+++ all_tests_report.sh
@@ -1,1 +1,1 @@
-curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"specificRecipient\": \"yuriy.cherepovskiy@applitools.com;serhii.khalymon@applitools.com\"}"
+curl http://sdk-test-results.herokuapp.com/send_full_regression/sdks -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\"}"
