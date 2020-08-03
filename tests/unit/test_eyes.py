eec7b3de722b0bff66ad9b51710249086f14fde8
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-08-03 18:30:25 +0300

    test: add server connector assign test

--- tests/unit/test_eyes.py
+++ tests/unit/test_eyes.py
@@ -1,5 +1,7 @@
 import pytest
 
+from applitools.core import ServerConnector
+
 
 def pytest_generate_tests(metafunc):
     if "eyes" in metafunc.fixturenames:
@@ -17,3 +19,20 @@ def test_is_disabled_False(eyes):
     with pytest.raises(Exception):
         eyes.is_disabled = False
         eyes.check(None, None)
+
+
+def test_set_get_server_connector(eyes):
+    class CustomServerCon(ServerConnector):
+        pass
+
+    conn = CustomServerCon()
+    eyes.server_connector = conn
+    assert eyes.server_connector is conn
+
+
+def test_set_incorrect_server_connector(eyes):
+    class CustomServerCon:
+        pass
+
+    with pytest.raises(ValueError):
+        eyes.server_connector = CustomServerCon()
