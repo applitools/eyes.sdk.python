17e4515c71c353aba3b8420c7cd6d71579a7528d
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-09-03 12:18:08 +0300

    test: update test_ufg_options

--- tests/functional/eyes_selenium/visual_grid/test_ufg_options.py
+++ tests/functional/eyes_selenium/visual_grid/test_ufg_options.py
@@ -1,8 +1,7 @@
-from mock import MagicMock
+from mock import patch
 
 from applitools.common import VisualGridOption
 from applitools.common.selenium import BrowserType
-from applitools.core import ServerConnector
 from applitools.selenium import VisualGridRunner, Eyes, Target
 
 
@@ -19,12 +18,23 @@ def test_ufg_options(driver):
     )
 
     driver.get("https://google.com")
-    eyes.open(driver, "Mock app", "Mock Test")
-    eyes.check(
-        "",
-        Target.window().visual_grid_options(
-            VisualGridOption("option3", "value3"), VisualGridOption("option4", 5)
-        ),
-    )
-    eyes.close_async()
-    res = runner.get_all_test_results()
+    with patch("applitools.core.server_connector.ServerConnector.render",) as patched:
+        eyes.open(driver, "Mock app", "Mock Test")
+        eyes.check(
+            "",
+            Target.window().visual_grid_options(
+                VisualGridOption("option3", "value3"),
+                VisualGridOption("option4", 5),
+                VisualGridOption("option1", 5),
+            ),
+        )
+        eyes.close_async()
+        res = runner.get_all_test_results()
+
+        request_options = patched.call_args.args[0].options
+        assert request_options == {
+            "option1": 5,
+            "option2": False,
+            "option3": "value3",
+            "option4": 5,
+        }
