ef51358ad512a170ce82a53788e86cf63aa48cdb
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-09-14 20:05:07 +0300

    style: fix with pre-commit

--- sendMail.sh
+++ sendMail.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"version\":\"$2\", \"changeLog\":\"$3\", \"testCoverageGap\":\"$4\"}"
-  
