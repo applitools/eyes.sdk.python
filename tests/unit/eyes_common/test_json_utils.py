db8042b7552a7a6c174a4d17080c9271d8ab7787
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-09-16 23:36:38 +0300

    test: add test_multithread_seialization

--- tests/unit/eyes_common/test_json_utils.py
+++ tests/unit/eyes_common/test_json_utils.py
@@ -1,8 +1,13 @@
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 
 from applitools.common import (
+    AppOutput,
+    ImageMatchSettings,
+    MatchWindowData,
+    Options,
     RectangleSize,
     Region,
     RenderInfo,
@@ -40,7 +45,6 @@ from tests.utils import get_resource
     ],
 )
 def test_render_request_serialize(browser_type):
-
     request_resources = {"url": VGResource.EMPTY("some-url.com")}
     dom_url = "dom-url.com"
     r_info = RenderInfo(
@@ -85,6 +89,48 @@ def test_running_session_serialization_and_deserialization():
     rs_json = json_utils.to_json(rs)
     assert '"isNew": true' in rs_json
     assert rs == json_utils.attr_from_json(rs_json, RunningSession)
+
+
+def test_multithread_seialization():
+    mwd = MatchWindowData(
+        agent_setup=None,
+        app_output=AppOutput(
+            dom_url="https://eyespublicwusi0.core/bl9crP4n81pwT9anq3r1Xr8g0e97eMliN8f7etrM110",
+            screenshot_url="https://eyespublicwusi0.blob.core/g0e97eMliN8f7etrM110",
+            viewport=RectangleSize(height=1300, width=2560),
+            title="",
+            screenshot_bytes=None,
+        ),
+        ignore_mismatch=False,
+        user_inputs=[],
+        tag="Capture",
+        render_id="bf3ce18291b4aa933bee",
+        options=Options(
+            force_match=False,
+            force_mismatch=False,
+            ignore_mismatch=False,
+            ignore_match=False,
+            image_match_settings=ImageMatchSettings(
+                ignore_regions=[
+                    Region(height=41, left=30, top=165, width=738),
+                    Region(height=32, left=30, top=480, width=738),
+                ]
+            ),
+            name="Capture",
+            render_id="bf3ce18291b4aa933bee",
+            replace_last=False,
+            source="applitoolscasppe.cm",
+            user_inputs=[],
+        ),
+    )
+    match_window_data = get_resource("unit/matchData.json").decode("utf-8")
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(json_utils.to_json, [mwd] * 10)
+    for i, r in enumerate(results):
+        assert json_utils.attr_from_json(
+            json_utils.to_json(mwd), MatchWindowData
+        ) == json_utils.attr_from_json(match_window_data, MatchWindowData)
+        assert "ignoreRegions" not in r
 
 
 def test_test_results_deserialization():
