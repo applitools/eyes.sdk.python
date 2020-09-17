64c10843c2c9ee2f99f2df2dc802bcf948e690c1
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-09-17 21:33:42 +0300

    fix: make it work

--- eyes_selenium/applitools/selenium/capture/dom_capture.py
+++ eyes_selenium/applitools/selenium/capture/dom_capture.py
@@ -26,8 +26,14 @@ if tp.TYPE_CHECKING:
     from applitools.selenium.webdriver import EyesWebDriver
 
 __all__ = ("get_full_window_dom",)
-_CAPTURE_FRAME_SCRIPT = get_resource("captureDomAndPoll.js")
+_CAPTURE_FRAME_SCRIPT = (
+    get_resource("captureDomAndPoll.js") + "return __captureDomAndPoll();"
+)
+_CAPTURE_FRAME_SCRIPT_IE = (
+    get_resource("captureDomAndPollForIE.js") + "return __captureDomAndPollForIE();"
+)
 DOM_EXTRACTION_TIMEOUT = 5 * 60 * 1000
+CSS_DOWNLOAD_TIMEOUT = 3  # Secs
 
 
 @attr.s
@@ -39,54 +45,111 @@ class Separator:
     iframe_end_token = attr.ib(metadata={JsonInclude.THIS: True})
 
 
-def _parse_script_result(script_result, missing_css, missing_frames, data):
-    # type: (Text, List, List, List) -> Separator
+class CssDownloader:
+    def __init__(self):
+        self.css_urls = []
+
+    def fetch_css_files(self, css_start_token, css_end_token, urls):
+        self.css_start_token = css_start_token
+        self.css_end_token = css_end_token
+        self.css_urls.extend(urls)
+
+    def results(self):
+        return {
+            url: clean_for_json(_process_raw_css_node(CssNode(None, url, None)))
+            for url in self.css_urls
+        }
+
+
+def _parse_script_result(script_result):
+    # type: (Text) -> (Separator, List, List, List)
+    missing_css, missing_frames, data = [], [], []
     lines = re.split(r"\r?\n", script_result)
-    try:
-        separators = json_utils.attr_from_json(lines[0], Separator)
-        blocks = [missing_css, missing_frames, data]
-        block_index = 0
-        for line in lines[1:]:
-            if separators.separator == line:
-                block_index += 1
-            else:
-                blocks[block_index].append(line)
-        logger.info("missing css count: {}".format(len(missing_css)))
-        logger.info("missing frames count: {}".format(len(missing_frames)))
-    except Exception as e:
-        logger.exception(e)
-    # TODO: probably need to add shouldWaitForPhaser
-    return separators
+    separators = json_utils.attr_from_json(lines[0], Separator)
+    blocks = [missing_css, missing_frames, data]
+    block_index = 0
+    for line in lines[1:]:
+        if separators.separator == line:
+            block_index += 1
+        else:
+            blocks[block_index].append(line)
+    logger.info("missing css count: {}".format(len(missing_css)))
+    logger.info("missing frames count: {}".format(len(missing_frames)))
+    return separators, missing_css, missing_frames, data
 
 
-def recurse_frames(frame_data):
-    if not frame_data:
-        return ""
+def recurse_frames(driver, missing_frames_list, css_downoader):
+    frame_data = {}
+    switch_to = driver.switch_to
+    fc = driver.frame_chain.clone()
+    for missing_frame_line in missing_frames_list:
+        logger.info("Switching to frame line: {}".format(missing_frame_line))
+        original_location = driver.execute_script("return document.location.href")
+        try:
+            for missing_frame_xpath in missing_frame_line.split(","):
+                logger.info("Switching to specific frame: " + missing_frame_xpath)
+                frame = driver.find_element_by_xpath(missing_frame_xpath)
+                logger.info(
+                    "Switched to frame({}) with src ({})".format(
+                        missing_frame_xpath, frame.get_attribute("src")
+                    )
+                )
+                switch_to.frame(frame)
+                location_after_switch = driver.execute_script(
+                    "return document.location.href"
+                )
+                if location_after_switch == original_location:
+                    logger.info("Switching to frame failed")
+                    frame_data[missing_frame_line] = ""
+                    continue
+                frame_data[missing_frame_line] = get_frame_dom(driver, css_downoader)
+        except Exception:
+            logger.exception("Failed to get frame dom")
+            frame_data[missing_frame_line] = ""
+        switch_to.frames(fc)
+    return frame_data
 
 
 def efficient_string_replace(ref_id_open_token, ref_id_end_token, input, replacements):
-    if not replacements:
-        return input
+    res = input
+    for ph, replacement in replacements.items():
+        res = res.replace(ref_id_open_token + ph + ref_id_end_token, replacement)
+    return res
 
 
-def get_frame_dom(driver):
-    # type: (EyesWebDriver) -> Dict
+def clean_for_json(s):
+    # make json array and remove [" and "]
+    return json.dumps([s])[2:-2]
+
+
+def get_frame_dom(driver, css_downoader):
+    # type: (EyesWebDriver, CssDownloader) -> Dict
     script_result = eyes_selenium_utils.get_dom_script_result(
-        driver,
-        DOM_EXTRACTION_TIMEOUT,
-        "DomCapture_StopWatch",
-        _CAPTURE_FRAME_SCRIPT + "return __captureDomAndPoll();",
+        driver, DOM_EXTRACTION_TIMEOUT, "DomCapture_StopWatch", _CAPTURE_FRAME_SCRIPT,
     )
-    missing_css = []
-    missing_frames = []
-    data = []
-    separators = _parse_script_result(script_result, missing_css, missing_frames, data)
+    separators, missing_css, missing_frames, data = _parse_script_result(script_result)
 
-    # TODO: add fetch css func
-    # fetch_css_files(missing_css)
-    frame_data = recurse_frames(missing_frames)
+    css_downoader.fetch_css_files(
+        separators.css_start_token, separators.css_end_token, missing_css
+    )
+    frame_data = recurse_frames(driver, missing_frames, css_downoader)
     return efficient_string_replace(
         separators.iframe_start_token, separators.iframe_end_token, data[0], frame_data
+    )
+
+
+def get_dom(driver):
+    # type: (EyesWebDriver) -> Text
+    original_fc = driver.frame_chain.clone()
+    css_downloader = CssDownloader()
+    dom = get_frame_dom(driver, css_downloader)
+    if original_fc is not None:
+        driver.switch_to.frames(original_fc)
+    return efficient_string_replace(
+        css_downloader.css_start_token,
+        css_downloader.css_end_token,
+        dom,
+        css_downloader.results(),
     )
 
 
@@ -99,7 +162,7 @@ def get_full_window_dom(driver, return_as_dict=False):
         ScrollPositionProvider(driver, current_root_element)
     ):
         logger.debug("Traverse DOM Tree")
-        script_result = get_frame_dom(driver)
+        script_result = get_dom(driver)
 
     return json.loads(script_result) if return_as_dict else script_result
 
