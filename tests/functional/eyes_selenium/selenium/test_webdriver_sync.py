261f92d69de740cdd71e416f60692ae283c4b1f9
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-10-08 11:16:50 +0300

    test: add test_measure_ensure_sync_with_underlying_driver_performance

--- tests/functional/eyes_selenium/selenium/test_webdriver_sync.py
+++ tests/functional/eyes_selenium/selenium/test_webdriver_sync.py
@@ -1,3 +1,7 @@
+import time
+
+import pytest
+
 from applitools.selenium import EyesWebDriver
 
 
@@ -24,3 +28,55 @@ def test_iframe_unselected_with_raw_selenium_driver_is_synced(eyes, driver):
     eyes_driver.ensure_sync_with_underlying_driver()
 
     assert eyes_driver.frame_chain.peek is None
+
+
+@pytest.mark.parametrize("i", range(5))
+@pytest.mark.parametrize(
+    "test_desc,driver_frames,eyes_frames",
+    [
+        ("No frame selected; sync", [], []),
+        ("Frame selected; sync", [], [0]),
+        ("Frame selected; out of sync", [0], []),
+        ("Far frame selected; sync", [], "far"),
+        ("Far frame selected; out of sync", "far", []),
+    ],
+)
+@pytest.mark.parametrize(
+    "page_desc,url,far_frame_path,load_time",
+    [
+        (
+            "Light",
+            "https://applitools.github.io/demo/TestPages/CorsTestPage/index.html",
+            (0, 0),
+            1,
+        ),
+        ("Complex", "https://smiledirectclub.com/invite/", (7, 2), 10),
+    ],
+)
+@pytest.mark.skip("Currently it is designed for manual performance measurement")
+def test_measure_ensure_sync_with_underlying_driver_performance(
+    driver,
+    eyes,
+    page_desc,
+    url,
+    far_frame_path,
+    load_time,
+    test_desc,
+    driver_frames,
+    eyes_frames,
+    i,
+):
+    driver_frames = far_frame_path if driver_frames == "far" else driver_frames
+    eyes_frames = far_frame_path if eyes_frames == "far" else eyes_frames
+    driver.get(url)
+    time.sleep(load_time)
+    eyes_driver = EyesWebDriver(driver, eyes)
+    for n in eyes_frames:
+        eyes_driver.switch_to.frame(n)
+    for n in driver_frames:
+        driver.switch_to.frame(n)
+    ts = time.monotonic()
+    eyes_driver.ensure_sync_with_underlying_driver()
+    r = time.monotonic() - ts
+    with open("ensure_sync_with_underlying_driver_perf.txt", "a") as f:
+        f.write(f"{page_desc}; {test_desc}; {i}; {r * 1000:.0f}\n")
