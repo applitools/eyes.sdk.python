997ccf9229d8237acee2592acf00588c2808b4fc
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-10-22 21:17:34 +0300

    test: test that cached resources actually get into render request

---
+++ tests/unit/eyes_selenium/visual_grid/test_render_task.py
@@ -0,0 +1,47 @@
+from applitools.selenium.visual_grid import RenderTask, ResourceCache
+from tests.unit.eyes_core.test_server_connector import MockResponse
+
+
+def test_cached_child_resources_get_into_render_request():
+    def mock_css(content=b"", type="text/css", code=200):
+        return MockResponse(None, content, code, {"Content-Type": type})
+
+    internet = {
+        "https://a.com/1.css": mock_css(b'@import url(2.css);@import "3.css"'),
+        "https://a.com/2.css": mock_css(b'@import "4.css"'),
+        "https://a.com/3.css": mock_css(code=404),
+        "https://a.com/4.css": mock_css(b""),
+    }
+
+    class ConnectorMock:
+        @staticmethod
+        def download_resource(url):
+            return internet[url]
+
+    capture_result = {
+        "cdt": [],
+        "url": "https://a.com/a",
+        "resourceUrls": ["https://a.com/1.css"],
+    }
+    cache = ResourceCache()
+    task = RenderTask("A", {}, cache, None, ConnectorMock, None)
+
+    vg_dom1 = task.parse_frame_dom_resources(capture_result)
+    assert set(vg_dom1.resources) == {
+        "https://a.com/1.css",
+        "https://a.com/2.css",
+        "https://a.com/3.css",
+        "https://a.com/4.css",
+    }
+    assert "https://a.com/1.css" in cache
+    assert "https://a.com/2.css" in cache
+    assert "https://a.com/3.css" in cache
+    assert "https://a.com/4.css" in cache
+
+    vg_dom2 = task.parse_frame_dom_resources(capture_result)
+    assert set(vg_dom2.resources) == {
+        "https://a.com/1.css",
+        "https://a.com/2.css",
+        "https://a.com/3.css",
+        "https://a.com/4.css",
+    }
