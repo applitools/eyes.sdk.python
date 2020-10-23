0430dd93bc57ba0e248085e08410597f7aa8e851
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-10-23 13:54:04 +0300

    test: recursive resources downloaded only once

--- tests/unit/eyes_selenium/visual_grid/test_render_task.py
+++ tests/unit/eyes_selenium/visual_grid/test_render_task.py
@@ -45,3 +45,38 @@ def test_cached_child_resources_get_into_render_request():
         "https://a.com/3.css",
         "https://a.com/4.css",
     }
+
+
+def test_recursive_resources_downloaded_once():
+    def mock_css(content=b"", type="text/css", code=200):
+        return MockResponse(None, content, code, {"Content-Type": type})
+
+    internet = {
+        "https://a.com/1.css": mock_css(b"@import url(2.css);"),
+        "https://a.com/2.css": mock_css(b"@import url(1.css);"),
+    }
+
+    class ConnectorMock:
+        call_count = 0
+
+        @staticmethod
+        def download_resource(url):
+            ConnectorMock.call_count += 1
+            return internet[url]
+
+    capture_result = {
+        "cdt": [],
+        "url": "https://a.com/a",
+        "resourceUrls": ["https://a.com/1.css"],
+    }
+    cache = ResourceCache()
+    task = RenderTask("A", {}, cache, None, ConnectorMock, None)
+
+    vg_dom1 = task.parse_frame_dom_resources(capture_result)
+    assert ConnectorMock.call_count == 2
+    assert set(vg_dom1.resources) == {
+        "https://a.com/1.css",
+        "https://a.com/2.css",
+    }
+    assert "https://a.com/1.css" in cache
+    assert "https://a.com/2.css" in cache
