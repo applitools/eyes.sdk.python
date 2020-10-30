075c7c01b2d4869c1222176c7e5a424a45de0416
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-10-30 21:52:02 +0200

    test: check that urls are correctly extracted from css files

--- tests/unit/eyes_selenium/test_parsers.py
+++ tests/unit/eyes_selenium/test_parsers.py
@@ -21,3 +21,12 @@ def test_parse_valid_svg_with_bom():
 def test_parse_invalid_svg_with_comment_on_top():
     content = get_resource("fa-regular-400.svg")
     parsers.get_urls_from_svg_resource(content)
+
+
+def test_collect_urls_from_css():
+    content = b'@import "1.css";@import url(2.css);@import url("3.css")'
+    content_type = "text/css"
+
+    urls = parsers.collect_urls_from_(content_type, content)
+
+    assert list(urls) == ["1.css", "2.css", "3.css"]
