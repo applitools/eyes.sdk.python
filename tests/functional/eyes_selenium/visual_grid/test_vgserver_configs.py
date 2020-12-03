eb8d3e8fe7bf9872e560d4c12d96011c621b63e3
Serhii Khalymon <sergiykhalimon@gmail.com>
2020-12-03 14:24:34 +0200

    test: update test_vgdouble_close_no_check

--- tests/functional/eyes_selenium/visual_grid/test_vgserver_configs.py
+++ tests/functional/eyes_selenium/visual_grid/test_vgserver_configs.py
@@ -1,15 +1,11 @@
-import pytest
-
-from applitools.common import TestFailedError
 from applitools.selenium import Configuration, Eyes
 
 
-def test_vgdouble_close_no_check(driver, vg_runner, batch_info):
+def test_vgdouble_close_no_check(driver, vg_runner, batch_info, fake_connector_class):
     eyes = Eyes(vg_runner)
     eyes.set_configuration(
         Configuration(app_name="app", test_name="test", batch=batch_info)
     )
+    eyes.server_connector = fake_connector_class()
     eyes.open(driver)
-    with pytest.raises(TestFailedError) as e:
-        eyes.close()
-        assert "Eyes not open" in str(e)
+    eyes.close()
