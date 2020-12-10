ac6f72b3036f1e39340ae81bfa4927dd7b148aca
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-12-10 20:14:19 +0200

    test: add test_put_cache_skips_already_cleared_resource

--- tests/unit/eyes_selenium/visual_grid/test_resource_cache.py
+++ tests/unit/eyes_selenium/visual_grid/test_resource_cache.py
@@ -4,12 +4,12 @@ from mock import MagicMock, call
 
 from applitools.selenium.visual_grid import PutCache
 
-DummyResource = namedtuple("DummyResource", "url, hash, hash_format")
+DummyResource = namedtuple("DummyResource", "url, hash, hash_format, content")
 DummyResource.clear = lambda _: None
 
 
 def test_put_cache_uploads_provided_url_once():
-    resources = [DummyResource("http://1", "hash1", "sha1")]
+    resources = [DummyResource("http://1", "hash1", "sha1", "a")]
     put_cache = PutCache()
     eyes_connector_mock = MagicMock()
     eyes_connector_mock.check_resource_status.side_effect = [[False], [True]]
@@ -19,12 +19,14 @@ def test_put_cache_uploads_provided_url_once():
     put_cache.shutdown()
     put_mock = eyes_connector_mock.render_put_resource
 
-    assert put_mock.call_args_list == [call(DummyResource("http://1", "hash1", "sha1"))]
+    assert put_mock.call_args_list == [
+        call(DummyResource("http://1", "hash1", "sha1", "a"))
+    ]
 
 
 def test_put_cache_uploads_same_url_with_different_hashes():
-    resources1 = [DummyResource("http://1", "hash1", "sha1")]
-    resources2 = [DummyResource("http://1", "hash2", "sha1")]
+    resources1 = [DummyResource("http://1", "hash1", "sha1", "a")]
+    resources2 = [DummyResource("http://1", "hash2", "sha1", "b")]
     put_cache = PutCache()
     eyes_connector_mock = MagicMock()
     eyes_connector_mock.check_resource_status.return_value = [False]
@@ -35,6 +37,19 @@ def test_put_cache_uploads_same_url_with_different_hashes():
     put_mock = eyes_connector_mock.render_put_resource
 
     assert put_mock.call_args_list == [
-        call(DummyResource("http://1", "hash1", "sha1")),
-        call(DummyResource("http://1", "hash2", "sha1")),
+        call(DummyResource("http://1", "hash1", "sha1", "a")),
+        call(DummyResource("http://1", "hash2", "sha1", "b")),
     ]
+
+
+def test_put_cache_skips_already_cleared_resource():
+    resources = [DummyResource("http://1", "hash1", "sha1", None)]
+    put_cache = PutCache()
+    eyes_connector_mock = MagicMock()
+    eyes_connector_mock.check_resource_status.return_value = [False]
+
+    put_cache.put(resources, eyes_connector_mock)
+    put_cache.shutdown()
+    put_mock = eyes_connector_mock.render_put_resource
+
+    assert put_mock.call_args_list == []
