fbea0eac8bee5db01d52ca1964c053c4c72d25cd
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2020-12-14 17:28:00 +0200

    fix: daemonize rendering service threads to allow application shutdown without waiting for test results

--- eyes_selenium/applitools/selenium/visual_grid/rendreing_service.py
+++ eyes_selenium/applitools/selenium/visual_grid/rendreing_service.py
@@ -16,7 +16,9 @@ class RenderingService(object):
         self._status_tasks = []
         self._have_status_tasks = Condition()
         self._render_request_thread = Thread(target=self._run_render_requests)
+        self._render_request_thread.daemon = True
         self._render_status_thread = Thread(target=self._run_render_status_requests)
+        self._render_status_thread.daemon = True
         self._render_request_thread.start()
         self._render_status_thread.start()
 
