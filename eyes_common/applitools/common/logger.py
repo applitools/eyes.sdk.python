49c1b494d51fb46aec55d405b013ef38d8329d60
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-01-14 17:15:09 +0200

    refactor: remove NullLogger

--- eyes_common/applitools/common/logger.py
+++ eyes_common/applitools/common/logger.py
@@ -9,7 +9,6 @@ import sys
 import threading
 import typing as tp
 import warnings
-from typing import Text
 
 import attr
 import structlog
@@ -17,25 +16,8 @@ import structlog.dev
 
 from applitools.common.utils.general_utils import get_env_with_prefix
 
-__all__ = ("StdoutLogger", "FileLogger", "NullLogger")
+__all__ = ("StdoutLogger", "FileLogger")
 _DEFAULT_HANDLER_LEVEL = int(get_env_with_prefix("LOGGER_LEVEL", logging.INFO))
-
-
-@attr.s
-class NullLogger(object):
-    """
-    A simple logger class which does nothing (log messages are ignored).
-
-    :param name: unused
-    :param level: The log level (e.g., logging.DEBUG)
-    """
-
-    name = attr.ib(default=None)
-    level = attr.ib(default=_DEFAULT_HANDLER_LEVEL)  # type: int
-
-    def configure(self, std_logger):
-        # type: (logging.Logger) -> None
-        pass
 
 
 @attr.s
@@ -105,7 +87,7 @@ class ConsoleRenderer(structlog.dev.ConsoleRenderer):
 
 
 def set_logger(logger=None):
-    # type: (tp.Union[StdoutLogger, FileLogger, NullLogger]) -> None
+    # type: (tp.Union[StdoutLogger, FileLogger]) -> None
     std_logger = logging.getLogger(__name__)
     logger.configure(std_logger)
 
