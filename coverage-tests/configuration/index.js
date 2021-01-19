9e467106abb6b8ffb5092b83943ad802d69cd334
Yurii <yurii.cherepovskiy@applitools.com>
2021-01-19 17:03:20 +0200

    Set link to master branch of sdk.coverage.tests

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/movePythonToMaster2/python/emitter.js',
-    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/movePythonToMaster2/python/overrides.js',
-    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/movePythonToMaster2/python/template.hbs',
+    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/emitter.js',
+    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/overrides.js',
+    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs',
     ext: '.py',
     outPath: './test/coverage/generic'
 }
