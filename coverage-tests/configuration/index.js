562212f780a8b233b8e41b7a9f2a72aecc8bf80f
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-02-09 16:50:14 +0200

    fix: use relative paths in coverage-tests index.js

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: '/home/yura/Repositories/Python/4/eyes.sdk.python/coverage-tests/emitter.js',
-    overrides: '/home/yura/Repositories/Python/4/eyes.sdk.python/coverage-tests/overrides.js',
+    emitter: '../../../../../emitter.js',
+    overrides: '../../../../../overrides.js',
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs',
     ext: '.py',
     outPath: './test/coverage/generic'
