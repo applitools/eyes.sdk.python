db1380e29937eebbfd548b8c751e456d4aa35b3b
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-02-11 23:40:42 +0200

    test: add test_extract_text

--- tests/functional/eyes_selenium/selenium/test_visual_locator.py
+++ tests/functional/eyes_selenium/selenium/test_visual_locator.py
@@ -1,8 +1,10 @@
 import pytest
+from selenium.webdriver.common.by import By
 
 from applitools.common import Region
 from applitools.core import TextRegionSettings, VisualLocator
 from applitools.selenium import ClassicRunner, Eyes, VisualGridRunner
+from applitools.selenium.text_regions import OCRRegion
 
 
 @pytest.mark.parametrize("eyes_runner", [ClassicRunner(), VisualGridRunner(1)])
@@ -23,10 +25,10 @@ def test_visual_locator(driver, eyes_runner):
 
 # TODO: Remove after merge of generated tests
 @pytest.mark.parametrize("eyes_runner", [ClassicRunner()])
-def test_text_regions(driver, eyes_runner):
+def test_extract_text_regions(driver, eyes_runner):
     driver.get("https://applitools.github.io/demo/TestPages/OCRPage")
     eyes = Eyes(eyes_runner)
-    test_name = "testTextRegions"
+    test_name = "testExtractTextRegions"
     if isinstance(eyes_runner, VisualGridRunner):
         test_name += "_VG"
 
@@ -53,3 +55,43 @@ def test_text_regions(driver, eyes_runner):
         regions[patterns[2]][0].text
         == "Incididum minim ad occaecat mollit sint elit ipsum. Consectetur eiusmod sint officia labore elit nostrud mollit eiusmod"
     )
+
+
+@pytest.mark.parametrize("eyes_runner", [ClassicRunner()])
+def test_extract_text(driver, eyes_runner):
+    driver.get("https://applitools.github.io/demo/TestPages/FramesTestPage/")
+    eyes = Eyes(eyes_runner)
+    test_name = "testExtractText"
+    if isinstance(eyes_runner, VisualGridRunner):
+        test_name += "_VG"
+    expected = [
+        """ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
+BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLI
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"""
+    ]
+    eyes.open(driver, "Applitools Eyes SDK", test_name)
+    element = driver.find_element_by_css_selector("#overflowing-div")
+    text_results = eyes.extract_text(OCRRegion(element))
+    assert len(text_results) == 1
+    assert text_results == expected
+
+    text_results = eyes.extract_text(OCRRegion([By.CSS_SELECTOR, "#overflowing-div"]))
+    assert len(text_results) == 1
+    assert text_results == expected
+
+    text_results = eyes.extract_text(OCRRegion(Region(0, 0, 400, 800)))
+    assert len(text_results) == 1
+
+    eyes.close_async()
