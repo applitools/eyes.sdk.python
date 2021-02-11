3346e96f3431a88925afe40f13c80d1cb7bd74a2
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-02-11 23:31:03 +0200

    feat: add get_inner_text and get_element_from_check_settings

--- eyes_selenium/applitools/selenium/eyes_selenium_utils.py
+++ eyes_selenium/applitools/selenium/eyes_selenium_utils.py
@@ -1,9 +1,6 @@
 from __future__ import absolute_import
 
-import json
-import threading
 import typing as tp
-from collections import OrderedDict
 from contextlib import contextmanager
 
 from appium.webdriver import Remote as AppiumWebDriver
@@ -14,6 +11,8 @@ from selenium.webdriver.remote.webelement import WebElement
 from applitools.common import EyesError, Point, RectangleSize, logger
 from applitools.common.utils import datetime_utils
 from applitools.common.utils.compat import urlparse
+
+from .webelement import EyesWebElement
 
 if tp.TYPE_CHECKING:
     from typing import Any, Dict, Generator, Optional, Text, Union
@@ -29,7 +28,6 @@ if tp.TYPE_CHECKING:
     from applitools.selenium.frames import FrameChain
     from applitools.selenium.positioning import SeleniumPositionProvider
     from applitools.selenium.webdriver import EyesWebDriver
-    from applitools.selenium.webelement import EyesWebElement
 
 __all__ = (
     "get_current_frame_content_entire_size",
@@ -563,3 +561,21 @@ def get_webapp_domain(driver):
 
 def get_check_source(driver):
     return get_app_name(driver) if is_mobile_app(driver) else get_webapp_domain(driver)
+
+
+def get_element_from_check_settings(driver, check_settings):
+    # type: (EyesWebDriver, SeleniumCheckSettings) -> EyesWebElement
+    if check_settings.values.target_element:
+        element = check_settings.values.target_element
+    elif check_settings.values.target_selector:
+        by, value = check_settings.values.target_selector
+        element = driver.find_element(by, value)
+    return EyesWebElement(element, driver)
+
+
+def get_inner_text(driver, element):
+    # type: (EyesWebDriver, EyesWebElement) -> Optional[Text]
+    try:
+        return driver.execute_script("return arguments[0].innerText", element)
+    except WebDriverException:
+        return None
