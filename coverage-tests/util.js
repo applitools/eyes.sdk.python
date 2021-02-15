375832b659ca84c2e033c1f2ef0550a46d323ba3
Yurii <yurii.cherepovskiy@applitools.com>
2021-02-15 23:45:24 +0200

    Remove local .js files

--- coverage-tests/util.js
+++
@@ -1,6 +0,0 @@
-function capitalizeFirstLetter(string) {
-    return string.toString().charAt(0).toUpperCase() + string.toString().slice(1);
-}
-module.exports = {
-    capitalizeFirstLetter: capitalizeFirstLetter,
-}
