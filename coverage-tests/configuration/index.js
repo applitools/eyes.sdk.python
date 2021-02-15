e78f8d0dd849db0d613b85451eda5726f1fed9be
Yurii <yurii.cherepovskiy@applitools.com>
2021-02-15 22:32:51 +0200

    Set link to branch with test overrides.js

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: '../../../../../emitter.js',
-    overrides: '../../../../../overrides.js',
+    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/emitter.js',
+    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/test_python_overrider/python/overrides.js',
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs',
     ext: '.py',
     outPath: './test/coverage/generic'
