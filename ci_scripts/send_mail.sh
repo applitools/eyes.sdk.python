af87512d8907c6b730a09f9306e7f9b2ee697d71
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-02-25 14:53:40 +0200

    chore: revert back changes in travis for deploy

--- ci_scripts/send_mail.sh
+++ ci_scripts/send_mail.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"version\":\"$2\", \"changeLog\":\"$3\", \"testCoverageGap\":\"$4\", \"specificRecipient\":\"serhii.khalymon@applitools.com\"}"
+curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"version\":\"$2\", \"changeLog\":\"$3\", \"testCoverageGap\":\"$4\"}"
