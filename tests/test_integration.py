ae06ddbf9d310385955ba54bfd44d2b6a0a03ec0
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-03-24 14:51:55 +0200

    chore: replace pytest-virtualenv with pytest-venv

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,5 @@
 import os.path
+import subprocess
 
 here = os.path.dirname(os.path.join(__file__))
 root_dir = os.path.normpath(os.path.join(here, os.pardir))
@@ -25,64 +26,56 @@ def _packages_resolver(common=False, core=False, selenium=False, images=False):
     return str(os.path.join(root_dir, pack))
 
 
-def test_setup_eyes_common(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    assert "eyes-common" in virtualenv.installed_packages()
+def test_setup_eyes_common(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    assert venv.get_version("eyes-common")
 
 
-def test_setup_eyes_core(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    assert "eyes-common" in virtualenv.installed_packages()
-    assert "eyes-core" in virtualenv.installed_packages()
+def test_setup_eyes_core(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    assert venv.get_version("eyes-common")
+    assert venv.get_version("eyes-core")
 
 
-def test_setup_eyes_images(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(images=True), build_egg=True)
-    assert "eyes-common" in virtualenv.installed_packages()
-    assert "eyes-core" in virtualenv.installed_packages()
-    assert "eyes-images" in virtualenv.installed_packages()
+def test_setup_eyes_images(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    venv.install(_packages_resolver(images=True), editable=True)
+    assert venv.get_version("eyes-common")
+    assert venv.get_version("eyes-core")
+    assert venv.get_version("eyes-images")
 
 
-def test_setup_eyes_selenium(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(selenium=True), build_egg=True)
-    assert "eyes-common" in virtualenv.installed_packages()
-    assert "eyes-core" in virtualenv.installed_packages()
-    assert "eyes-selenium" in virtualenv.installed_packages()
+def test_setup_eyes_selenium(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    venv.install(_packages_resolver(selenium=True), editable=True)
+    assert venv.get_version("eyes-common")
+    assert venv.get_version("eyes-core")
+    assert venv.get_version("eyes-selenium")
 
 
-def test_eyes_common_namespace_package(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.run('python -c "from applitools.common import *"')
+def test_eyes_common_namespace_package(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    subprocess.check_call([venv.python, "-c", "from applitools.common import *"])
 
 
-def test_eyes_core_namespace_package(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    virtualenv.run('python -c "from applitools.core import *"')
+def test_eyes_core_namespace_package(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    subprocess.check_call([venv.python, "-c", "from applitools.core import *"])
 
 
-def test_eyes_images_namespace_package(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(images=True), build_egg=True)
-    virtualenv.run('python -c "from applitools.images import *"')
+def test_eyes_images_namespace_package(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    venv.install(_packages_resolver(images=True), editable=True)
+    subprocess.check_call([venv.python, "-c", "from applitools.images import *"])
 
 
-def test_eyes_selenium_namespace_package(virtualenv):
-    virtualenv.install_package("certifi")
-    virtualenv.install_package(_packages_resolver(common=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(core=True), build_egg=True)
-    virtualenv.install_package(_packages_resolver(selenium=True), build_egg=True)
-    virtualenv.run('python -c "from applitools.selenium import *"')
+def test_eyes_selenium_namespace_package(venv):
+    venv.install(_packages_resolver(common=True), editable=True)
+    venv.install(_packages_resolver(core=True), editable=True)
+    venv.install(_packages_resolver(selenium=True), editable=True)
+    subprocess.check_call([venv.python, "-c", "from applitools.selenium import *"])
