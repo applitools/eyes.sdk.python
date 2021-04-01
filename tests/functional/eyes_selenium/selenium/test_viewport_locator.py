b3b3d6fd97cdabbdae059370aa755baa0ffd7de3
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-04-01 13:30:52 +0200

    test: test data from JS SDK

--- tests/functional/eyes_selenium/selenium/test_viewport_locator.py
+++ tests/functional/eyes_selenium/selenium/test_viewport_locator.py
@@ -1,5 +1,4 @@
-from os import path
-
+import pytest
 from PIL import Image
 
 from applitools.common import Point
@@ -10,6 +9,7 @@ from applitools.selenium.viewport_locator import (
     find_pattern,
     remove_page_marker,
 )
+from tests.utils import get_resource_path
 
 
 def test_add_remove_marker(driver, eyes):
@@ -28,15 +28,23 @@ def test_add_remove_marker(driver, eyes):
     assert marker == Pattern(2, 6, [0, 1, 0])
 
 
-def test_find_pattern():
-    marker = Pattern(2, 6, [0, 1, 0])
-    image = Image.open(
-        path.join(
-            path.dirname(path.realpath(__file__)),
-            "resources/browser_window_with_marker.png",
-        )
-    )
+@pytest.mark.parametrize(
+    "image_name, marker_location, pixel_ratio",
+    [
+        ("iPad_Air_portrait.png", Point(0, 140), 2),
+        ("iPhone_5S_landscape.png", Point(0, 100), 2),
+        ("iPhone_X_perfecto_portrait.png", Point(0, 297), 3),
+        ("iPhone_XR_perfecto_landscape.png", Point(88, 100), 2),
+        ("iPhone_XS_landscape.png", Point(132, 150), 3),
+        ("iPhone_XS_Max_perfecto_landscape.png", Point(132, 150), 3),
+        ("iPhone_XS_portrait_nomarker.png", None, 3),
+        ("iPhone_XS_portrait.png", Point(0, 282), 3),
+    ],
+)
+def test_find_pattern(image_name, marker_location, pixel_ratio):
+    marker = Pattern(pixel_ratio, 3 * pixel_ratio, [0, 1, 0])
+    image = Image.open(get_resource_path("unit/ios_screenshots/" + image_name))
 
     location = find_pattern(image, marker)
 
-    assert location == Point(112, 322)
+    assert location == marker_location
