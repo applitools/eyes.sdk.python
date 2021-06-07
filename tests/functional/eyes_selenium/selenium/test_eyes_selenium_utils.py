ef77f3846154ac53b2cb8e3f2c561f1a2371597e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-06-07 12:01:57 +0300

    test: add minimal browser sizes tests

--- tests/functional/eyes_selenium/selenium/test_eyes_selenium_utils.py
+++ tests/functional/eyes_selenium/selenium/test_eyes_selenium_utils.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
-from applitools.common import RectangleSize
+from applitools.common import EyesError, RectangleSize
 from applitools.selenium.eyes_selenium_utils import get_viewport_size, set_viewport_size
 
 
@@ -60,6 +60,57 @@ def sauce_chrome_w10(sauce_driver_url):
 
 
 @pytest.fixture
+def sauce_firefox_w10(sauce_driver_url):
+    capabilities = {
+        "browserName": "firefox",
+        "browserVersion": "latest",
+        "platformName": "Windows 10",
+        "sauce:options": {
+            "screenResolution": "1024x768",
+        },
+    }
+    driver = webdriver.Remote(sauce_driver_url, capabilities)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+@pytest.fixture
+def sauce_chrome_macos(sauce_driver_url):
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "latest",
+        "platformName": "macOS 11.00",
+        "sauce:options": {
+            "screenResolution": "1024x768",
+        },
+    }
+    driver = webdriver.Remote(sauce_driver_url, capabilities)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+@pytest.fixture
+def sauce_firefox_macos(sauce_driver_url):
+    capabilities = {
+        "browserName": "firefox",
+        "browserVersion": "latest",
+        "platformName": "macOS 11.00",
+        "sauce:options": {
+            "screenResolution": "1024x768",
+        },
+    }
+    driver = webdriver.Remote(sauce_driver_url, capabilities)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+@pytest.fixture
 def sauce_safari11_osx1013(sauce_driver_url):
     capabilities = {
         "browserName": "safari",
@@ -82,6 +133,19 @@ def sauce_safari12_osx1013_legacy(sauce_driver_url):
         "browserName": "safari",
         "platform": "macOS 10.13",
         "version": "12.1",
+    }
+    driver = webdriver.Remote(sauce_driver_url, capabilities)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+@pytest.fixture
+def sauce_safari_latest(sauce_driver_url):
+    capabilities = {
+        "browserName": "safari",
+        "version": "latest",
     }
     driver = webdriver.Remote(sauce_driver_url, capabilities)
     try:
@@ -185,3 +249,37 @@ def test_driver_safari12_osx1013_set_window_rect_fails(sauce_safari12_osx1013_le
     }
     with pytest.raises(WebDriverException):
         sauce_safari12_osx1013_legacy.set_window_rect(0, 0, 800, 600)
+
+
+def test_set_viewport_size_10_10_safari_latest(sauce_safari_latest):
+    set_viewport_size(sauce_safari_latest, RectangleSize(10, 10))
+
+    assert get_viewport_size(sauce_safari_latest) == RectangleSize(10, 10)
+
+
+def test_set_viewport_size_10_10_win_chrome(sauce_chrome_w10):
+    with pytest.raises(EyesError, match="Failed to set the viewport size"):
+        set_viewport_size(sauce_chrome_w10, RectangleSize(10, 10))
+
+    assert get_viewport_size(sauce_chrome_w10) == RectangleSize(500, 10)
+
+
+def test_set_viewport_size_10_10_mac_chrome(sauce_chrome_macos):
+    with pytest.raises(EyesError, match="Failed to set the viewport size"):
+        set_viewport_size(sauce_chrome_macos, RectangleSize(10, 10))
+
+    assert get_viewport_size(sauce_chrome_macos) == RectangleSize(500, 252)
+
+
+def test_set_viewport_size_10_10_win_firefox(sauce_firefox_w10):
+    with pytest.raises(EyesError, match="Failed to set the viewport size"):
+        set_viewport_size(sauce_firefox_w10, RectangleSize(10, 10))
+
+    assert get_viewport_size(sauce_firefox_w10) == RectangleSize(454, 43)
+
+
+def test_set_viewport_size_10_10_mac_firefox(sauce_firefox_macos):
+    with pytest.raises(EyesError, match="Failed to set the viewport size"):
+        set_viewport_size(sauce_firefox_macos, RectangleSize(10, 10))
+
+    assert get_viewport_size(sauce_firefox_macos) == RectangleSize(450, 38)
