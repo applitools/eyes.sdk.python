6a6573ce7d189af4dade6a2bafed807c9a7e3437
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-06-14 14:19:35 +0300

    test: add test_render_failure_aborts_session

--- tests/functional/eyes_selenium/visual_grid/test_visual_grid_runner.py
+++ tests/functional/eyes_selenium/visual_grid/test_visual_grid_runner.py
@@ -1,4 +1,4 @@
-from applitools.selenium import VisualGridRunner
+from applitools.selenium import Eyes, VisualGridRunner
 
 
 def test_visual_grid_runner_runner_started_logging(fake_connector_class):
@@ -10,3 +10,19 @@ def test_visual_grid_runner_runner_started_logging(fake_connector_class):
     event = connector.input_calls["send_logs"][0][0]
     assert event.level.value == 2
     assert event.event == '{"defaultConcurrency": 5, "type": "runnerStarted"}'
+
+
+def test_render_failure_aborts_session(driver, batch_info, vg_runner, spy):
+    driver.get("https://applitools.github.io/demo/TestPages/SimpleTestPage")
+    eyes = Eyes(vg_runner)
+    close_session_spy = spy(eyes.server_connector, "stop_session")
+
+    def broken(*args):
+        raise Exception
+
+    eyes.server_connector.render = broken
+    eyes.open(driver, "Eyes SDK", "UFG Render Error")
+    eyes.check_window()
+    eyes.close(False)
+
+    assert close_session_spy.call_args_list == [spy.call(spy.ANY, True, False)]
