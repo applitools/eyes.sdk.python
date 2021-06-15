0df441c9a746defa251c0f00b779a8f438a1553e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-06-15 14:25:53 +0300

    test: add test_snapshot_too_big_aborts_session

--- tests/functional/eyes_selenium/visual_grid/test_visual_grid_runner.py
+++ tests/functional/eyes_selenium/visual_grid/test_visual_grid_runner.py
@@ -1,3 +1,4 @@
+from applitools.common import RGridDom
 from applitools.selenium import Eyes, VisualGridRunner
 
 
@@ -21,6 +22,19 @@ def test_render_failure_aborts_session(driver, batch_info, vg_runner, spy):
         raise Exception
 
     eyes.server_connector.render = broken
+    eyes.open(driver, "Eyes SDK", "UFG Render Error")
+    eyes.check_window()
+    eyes.close(False)
+
+    assert close_session_spy.call_args_list == [spy.call(spy.ANY, True, False)]
+
+
+def test_snapshot_too_big_aborts_session(driver, vg_runner, spy, monkeypatch):
+    monkeypatch.setattr(RGridDom, "MAX_CDT_SIZE", 1)
+    driver.get("https://applitools.github.io/demo/TestPages/SimpleTestPage")
+    eyes = Eyes(vg_runner)
+    close_session_spy = spy(eyes.server_connector, "stop_session")
+
     eyes.open(driver, "Eyes SDK", "UFG Render Error")
     eyes.check_window()
     eyes.close(False)
