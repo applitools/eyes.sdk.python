89ee0bcdedebf40d6a0668a7367edca397e9390f
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-06-23 17:34:45 +0300

    Bugfix trello 2614 selenium mobile web css stiching error (#359)
    * test: add test_stitching_modes
    
    * fix: CSSMobileSafariPositionProvider with None target_element crashes _add_data_attribute_to_element
    
    * docs: update CHANGELOG.md

---
+++ tests/functional/eyes_selenium/mobile/test_stitching_modes.py
@@ -0,0 +1,50 @@
+from os import environ
+
+import pytest
+from selenium import webdriver
+
+from applitools.common import StitchMode
+from applitools.selenium import Eyes, Target
+
+
+@pytest.fixture
+def sauce_driver_url():
+    return "https://{}:{}@ondemand.saucelabs.com:443/wd/hub".format(
+        environ["SAUCE_USERNAME"], environ["SAUCE_ACCESS_KEY"]
+    )
+
+
+@pytest.fixture
+def sauce_iphone8_ios14_driver(sauce_driver_url):
+    capabilities = {
+        "browserName": "Safari",
+        "deviceName": "iPhone 8 Simulator",
+        "deviceOrientation": "portrait",
+        "platformName": "iOS",
+        "platformVersion": "14.3",
+    }
+    driver = webdriver.Remote(sauce_driver_url, capabilities)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def test_eyes_mobile_scroll_stitching(sauce_iphone8_ios14_driver, eyes):
+    sauce_iphone8_ios14_driver.get("https://demo.applitools.com/")
+    eyes.stitch_mode = StitchMode.Scroll
+    eyes.open(sauce_iphone8_ios14_driver, "Tests", "ios14 scroll stitching")
+
+    eyes.check("step", Target.window().fully())
+
+    eyes.close()
+
+
+def test_eyes_mobile_css_stitching(sauce_iphone8_ios14_driver, eyes):
+    sauce_iphone8_ios14_driver.get("https://demo.applitools.com/")
+    eyes.stitch_mode = StitchMode.CSS
+    eyes.open(sauce_iphone8_ios14_driver, "Tests", "ios14 css stitching")
+
+    eyes.check("step", Target.window().fully())
+
+    eyes.close()
