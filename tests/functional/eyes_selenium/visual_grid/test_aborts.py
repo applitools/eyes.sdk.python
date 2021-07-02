a169986771d4f8dc61a318fcf28aec9f1c2bd79e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-07-02 10:58:20 +0300

    test: abort and abort_async after close tests

--- tests/functional/eyes_selenium/visual_grid/test_aborts.py
+++ tests/functional/eyes_selenium/visual_grid/test_aborts.py
@@ -53,3 +53,31 @@ def test_abort_async_on_vg(driver, vg_runner, batch_info):
     time.sleep(15)
     eyes.abort_async()
     all_results = vg_runner.get_all_test_results(False)
+
+
+def test_abort_after_close_must_not_abort(driver, vg_runner, batch_info):
+    eyes = Eyes(vg_runner)
+    eyes.configure.test_name = "Test abort after close VG"
+    eyes.configure.app_name = "Test Abort_VG"
+    eyes.configure.batch = batch_info
+    eyes.configure.add_browser(800, 600, BrowserType.CHROME)
+    driver.get("data:text/html,<p>Test</p>")
+    eyes.open(driver)
+    eyes.check(Target.window())
+    eyes.close()
+    eyes.abort()
+    all_results = vg_runner.get_all_test_results()
+
+
+def test_abort_after_close_async_must_not_abort(driver, vg_runner, batch_info):
+    eyes = Eyes(vg_runner)
+    eyes.configure.test_name = "Test abort after close async VG"
+    eyes.configure.app_name = "Test Abort_VG"
+    eyes.configure.batch = batch_info
+    eyes.configure.add_browser(800, 600, BrowserType.CHROME)
+    driver.get("data:text/html,<p>Test</p>")
+    eyes.open(driver)
+    eyes.check(Target.window())
+    eyes.close_async()
+    eyes.abort()
+    all_results = vg_runner.get_all_test_results()
