3ca2acffb54882bc7abc4190c2917801d659479e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-07-05 16:18:35 +0300

    test: update test data

--- tests/functional/eyes_selenium/mobile/test_app_native_sauce_labs.py
+++ tests/functional/eyes_selenium/mobile/test_app_native_sauce_labs.py
@@ -143,4 +143,4 @@ def test_iOS_native_region_sauce_labs_tracking_id_sent(mobile_eyes):
         eyes.check("Contact list", Target.window())
         match_window_data = smw.call_args[0][1]  # type: MatchWindowData
 
-    assert match_window_data.options.source == "HelloWorldiOS_1_0.zip"
+    assert match_window_data.options.source == "eyes-ios-hello-world.zip"
