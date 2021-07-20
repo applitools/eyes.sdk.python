22cafcba22185e145dcb5e69ea753aaaa1214084
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-07-20 16:25:13 +0300

    Fix collect_check_settings if similar keywords calls multiple times

--- eyes_robot/src/EyesLibrary/utils.py
+++ eyes_robot/src/EyesLibrary/utils.py
@@ -5,6 +5,8 @@ from robot.libraries.BuiltIn import BuiltIn
 
 from applitools.selenium.fluent import SeleniumCheckSettings
 
+SEPARATOR = object()
+
 
 def extract_keyword_and_arguments(
     keywords_from_test,  # type: list[Any]|tuple[Any]
@@ -14,9 +16,15 @@ def extract_keyword_and_arguments(
     # need to reverse for return correct index with bisect
     res = defaultdict(list)
     key_keyword = None
+    key_keyword_index = -1
     for i, keyword in enumerate(keywords_from_test):
+        if keyword == key_keyword and key_keyword_index != i:
+            # Next keyword with similar name
+            res[key_keyword].append(SEPARATOR)
+
         if keyword in defined_keywords:
             key_keyword = keyword
+            key_keyword_index = i
             continue
         if key_keyword is None:
             raise ValueError(
@@ -27,10 +35,28 @@ def extract_keyword_and_arguments(
         yield keyword, arguments
 
 
-def collect_check_settings(check_settings, defined_keywords, *keywords):
-    # type: (SeleniumCheckSettings, list[str], tuple[Any]) -> SeleniumCheckSettings
+def splits_args_by_separator(args):
+    # type: (list[Any]) -> Generator[tuple[Any], None, None]
+    res = ()
+    for i, arg in enumerate(args, start=1):
+        if arg is not SEPARATOR:
+            res += (arg,)
+        else:
+            yield res
+            res = ()
+            continue
+
+        if len(args) <= i:
+            yield res
+
+
+def collect_check_settings(
+    check_settings, defined_keywords, *keywords, executor=BuiltIn().run_keyword
+):
+    # type: (SeleniumCheckSettings,list[str],tuple[Any],callable)->SeleniumCheckSettings
     """ Fill `check_setting` with data from keyword and return `check_settings`"""
     for keyword, args in extract_keyword_and_arguments(keywords, defined_keywords):
-        args += [check_settings]
-        BuiltIn().run_keyword(keyword, *args)
+        for separated_args in splits_args_by_separator(args):
+            separated_args += (check_settings,)
+            executor(keyword, *separated_args)
     return check_settings
