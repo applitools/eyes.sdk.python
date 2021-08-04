f1d295c486819e61071f36babda2b992d178db66
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-08-04 21:06:34 +0300

    test: fix test_abort_when_not_rendered test

--- tests/functional/eyes_selenium/visual_grid/test_aborts.py
+++ tests/functional/eyes_selenium/visual_grid/test_aborts.py
@@ -3,24 +3,27 @@ import time
 from mock import patch
 
 from applitools.common import RenderStatus, RenderStatusResults
+from applitools.core import ServerConnector
 from applitools.selenium import BrowserType, Eyes, Target
 
 
-def test_abort_when_not_rendered(driver, vg_runner, batch_info):
+def test_abort_when_not_rendered(driver, vg_runner, batch_info, monkeypatch):
+    def failed_status(self, *ids):
+        return [
+            RenderStatusResults(status=RenderStatus.ERROR, render_id=i) for i in ids
+        ]
+
+    monkeypatch.setattr(ServerConnector, "render_status_by_id", failed_status)
     eyes = Eyes(vg_runner)
     eyes.configure.test_name = "TestAbortWhenNotRendering"
     eyes.configure.app_name = "Visual Grid Render Test"
     eyes.configure.batch = batch_info
     eyes.configure.add_browser(1200, 800, BrowserType.CHROME)
     driver.get("https://demo.applitools.com")
-    with patch(
-        "applitools.core.server_connector.ServerConnector.render_status_by_id"
-    ) as rsbi:
-        rsbi.return_value = [RenderStatusResults(status=RenderStatus.ERROR)]
-        eyes.open(driver)
-        eyes.check(Target.window())
-        eyes.close_async()
-        all_results = vg_runner.get_all_test_results(False)
+    eyes.open(driver)
+    eyes.check(Target.window())
+    eyes.close_async()
+    all_results = vg_runner.get_all_test_results(False)
 
 
 def test_abort_when_dom_snapshot_error(driver, vg_runner, batch_info):
