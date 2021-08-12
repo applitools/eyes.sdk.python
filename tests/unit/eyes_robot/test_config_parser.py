a6a99f93358ff5d592931cf55fdecdbd3e0e9596
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-08-12 17:39:38 +0300

    Add test_viewport_size_trafaret to viewport in config

---
+++ tests/unit/eyes_robot/test_config_parser.py
@@ -0,0 +1,24 @@
+import pytest
+import trafaret as t
+from EyesLibrary.config_parser import ToEnumTrafaret, ViewPortTrafaret
+
+from applitools.common import RectangleSize
+from applitools.common.selenium import BrowserType, StitchMode
+
+
+def test_to_enum_trafaret():
+    assert BrowserType.CHROME == ToEnumTrafaret(BrowserType, "browser_type").check(
+        "CHROME"
+    )
+    assert StitchMode.CSS == ToEnumTrafaret(StitchMode, "stitch_mode").check("CSS")
+
+    with pytest.raises(t.DataError, match=r"Incorrect value for `browser_type`"):
+        ToEnumTrafaret(BrowserType, "browser_type").check("MissingBrowser")
+
+
+def test_viewport_size_trafaret():
+    expected_red = RectangleSize(400, 400)
+    res = ViewPortTrafaret().check({"width": 400, "height": 400})
+    assert res == expected_red
+    res = ViewPortTrafaret().check("[400 400]")
+    assert res == expected_red
