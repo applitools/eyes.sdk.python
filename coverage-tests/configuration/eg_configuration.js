ed6262e81dd78f26776a0933fee882434843acd5
artem <artem.borodavka@applitools.com>
2021-08-13 17:01:59 +0300

    codacy fix

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: 'eyes_selenium_python',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/emitter.js',
+    name: "eyes_selenium_python",
+    emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/emitter.js",
     overrides: [
-        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/overrides.js',
-        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/eg.overrides.js',
+        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/overrides.js",
+        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/eg.overrides.js",
     ],
-    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/template.hbs',
-    ext: '.py',
-    outPath: './test/coverage/generic',
+    template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/template.hbs",
+    ext: ".py",
+    outPath: "./test/coverage/generic",
 }
