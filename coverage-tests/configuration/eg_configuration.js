3edf58fd7b8438cf5c613d9d08bc587163dfb346
artem <artem.borodavka@applitools.com>
2021-08-16 12:17:08 +0300

    switch to master sdk.coverage.tests

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: "eyes_selenium_python",
-    emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/emitter.js",
+    emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/emitter.js",
     overrides: [
-        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/overrides.js",
-        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/eg.overrides.js",
+        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/overrides.js",
+        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/eg.overrides.js",
     ],
-    template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/eg_python/python/template.hbs",
+    template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs",
     ext: ".py",
     outPath: "./test/coverage/generic",
 };
