ae7953e726fc27841dc6ff33c0f3f87a68cf594b
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-09-20 14:09:58 +0300

    test: add mobile region fully tests

--- tests/functional/eyes_selenium/mobile/test_mobile_web_region.py
+++ tests/functional/eyes_selenium/mobile/test_mobile_web_region.py
@@ -23,6 +23,19 @@ def _region_test_flow(eyes):
     )
 
 
+def _region_fully_test_flow(eyes):
+    eyes.configure.stitch_mode = StitchMode.Scroll
+    eyes.check(
+        "region-scroll",
+        Target.region([By.TAG_NAME, "footer"]),
+    )
+    eyes.configure.stitch_mode = StitchMode.CSS
+    eyes.check(
+        "region-css",
+        Target.region([By.TAG_NAME, "footer"]).fully().timeout(0),
+    )
+
+
 @pytest.mark.platform("iOS")
 @pytest.mark.capabilities(
     **{
@@ -42,6 +55,25 @@ def test_region_capture_ios(eyes, driver):
     eyes.close()
 
 
+@pytest.mark.platform("iOS")
+@pytest.mark.capabilities(
+    **{
+        "platformName": "iOS",
+        "browserName": "Safari",
+        "deviceName": "iPhone 11",
+        "platformVersion": "13.4",
+    }
+)
+@pytest.mark.test_page_url(
+    "https://applitools.github.io/demo/TestPages/RegionOutOfViewport/"
+)
+def test_region_fully_capture_ios(eyes, driver):
+    eyes.configure.is_simulator = True
+    eyes.open(driver, "Mobile Web Tests", "TestRegionFullyCapture_iOS")
+    _region_fully_test_flow(eyes)
+    eyes.close()
+
+
 @pytest.mark.platform("Android")
 @pytest.mark.capabilities(
     **{
@@ -58,4 +90,23 @@ def test_region_capture_android(eyes, driver):
     eyes.configure.is_simulator = True
     eyes.open(driver, "Mobile Web Tests", "TestRegionCapture_Android")
     _region_test_flow(eyes)
+    eyes.close()
+
+
+@pytest.mark.platform("Android")
+@pytest.mark.capabilities(
+    **{
+        "platformName": "Android",
+        "browserName": "Chrome",
+        "deviceName": "Samsung Galaxy S9 HD GoogleAPI Emulator",
+        "platformVersion": "9.0",
+    }
+)
+@pytest.mark.test_page_url(
+    "https://applitools.github.io/demo/TestPages/RegionOutOfViewport/"
+)
+def test_region_fully_capture_android(eyes, driver):
+    eyes.configure.is_simulator = True
+    eyes.open(driver, "Mobile Web Tests", "TestRegionFullyCapture_Android")
+    _region_fully_test_flow(eyes)
     eyes.close()
