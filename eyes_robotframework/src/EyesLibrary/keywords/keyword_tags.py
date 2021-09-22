196bb5b7926e53a1f1560a4a8438c5198927e0e8
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-09-22 13:02:54 +0300

    docs: rename tag from `UFG Only` to `Ultrafast Grid only`
    #381

--- eyes_robotframework/src/EyesLibrary/keywords/keyword_tags.py
+++ eyes_robotframework/src/EyesLibrary/keywords/keyword_tags.py
@@ -1,5 +1,5 @@
 CHECK_SETTING = "Check Settings keyword"
-UFG_RELATED = "UFG only"
+UFG_RELATED = "Ultrafast Grid only"
 TARGET_SUPPORT = "Allow to pass Target keyword"
 CHECK_SETTINGS_SUPPORT = "Allow to pass Check Settings keywords"
 CHECK_FLOW = "Check flow"
