2f96d154cd7a058748e05a65c9d062d5df93b5e9
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-10-08 21:10:46 +0300

    test: use appium version 2.0.0rc2

--- eyes_selenium/setup.py
+++ eyes_selenium/setup.py
@@ -42,7 +42,7 @@ if sys.version_info[:2] <= (2, 7):
     install_requires.append("tinycss2==0.6.1")
 else:
     install_requires.append("Pillow >= 5.0.0")
-    install_requires.append("Appium-Python-Client>=0.4,<2")
+    install_requires.append("Appium-Python-Client==2.0.0rc2")
     install_requires.append("tinycss2>=0.6.1,<1.1.0")
 
 # using this way of defining instead of 'typing>=3.5.2; python_version<="3.4"'
