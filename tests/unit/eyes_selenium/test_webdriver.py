731774d41d4a5444666a7f9ee2ae47a6c3f9c68c
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-10-14 22:57:13 +0300

    test: add test_driver_app_useragent_lowercase_os

--- tests/unit/eyes_selenium/test_webdriver.py
+++ tests/unit/eyes_selenium/test_webdriver.py
@@ -17,3 +17,20 @@ def test_driver_useragent_splits_version(version, major, minor):
 
     assert webderiver.user_agent.os_major_version == major
     assert webderiver.user_agent.os_minor_version == minor
+
+
+def test_driver_app_useragent_lowercase_os():
+    driver_mock, eyes_mock = MagicMock(), MagicMock()
+    driver_mock.desired_capabilities = {
+        "app": "1.zip",
+        "platformName": "ios",
+        "platformVersion": "13.4",
+        "automationName": "XCUITest",
+        "appiumVersion": "1.17.1",
+        "deviceName": "iPhone XR Simulator",
+        "deviceOrientation": "portrait",
+    }
+    webderiver = EyesWebDriver(driver_mock, eyes_mock)
+
+    assert webderiver.user_agent.os_major_version == 13
+    assert webderiver.user_agent.os_minor_version == 4
