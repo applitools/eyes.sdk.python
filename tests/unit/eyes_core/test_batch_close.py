46a5c11cf78b54a740441db2933a367a6bbbf765
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-10-19 20:11:25 +0300

    test: add test_batch_closes_with_dont_closes_in_runner

--- tests/unit/eyes_core/test_batch_close.py
+++ tests/unit/eyes_core/test_batch_close.py
@@ -6,10 +6,11 @@ from mock import ANY, call, patch
 
 from applitools.common import ProxySettings
 from applitools.core import BatchClose
+from applitools.selenium import ClassicRunner, VisualGridRunner
 
 
 @pytest.mark.parametrize("env_dont_close", ["1", "True", "true", "false", "False"])
-def test_batch_no_api_key_error(env_dont_close):
+def test_batch_closes_with_dont_closes_env(env_dont_close):
     with patch.dict(
         os.environ,
         {
@@ -20,6 +21,26 @@ def test_batch_no_api_key_error(env_dont_close):
         with patch("requests.delete") as mocked_request:
             BatchClose().set_batch_ids("test-batch-id").close()
             if strtobool(env_dont_close):
+                assert not mocked_request.called
+            else:
+                assert mocked_request.call_args[0][0]
+
+
+@pytest.mark.parametrize("dont_close_batches", [True, False])
+@pytest.mark.parametrize("runner_cls", [ClassicRunner, lambda: VisualGridRunner(1)])
+def test_batch_closes_with_dont_closes_in_runner(dont_close_batches, runner_cls):
+    runner = runner_cls()
+    runner.set_dont_close_batches(dont_close_batches)
+
+    with patch.dict(
+        os.environ,
+        {
+            "APPLITOOLS_API_KEY": "Some-key",
+        },
+    ):
+        with patch("requests.delete") as mocked_request:
+            BatchClose().set_batch_ids("test-batch-id").close()
+            if dont_close_batches:
                 assert not mocked_request.called
             else:
                 assert mocked_request.call_args[0][0]
