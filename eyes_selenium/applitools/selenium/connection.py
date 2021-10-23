07e85233d2323513ef961d17615cbecfdca804b3
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-10-23 21:13:11 +0300

    feat: make USDK connection thread safe so eyes and runner can belong to different threads

--- eyes_selenium/applitools/selenium/connection.py
+++ eyes_selenium/applitools/selenium/connection.py
@@ -1,7 +1,9 @@
+import sys
+from concurrent.futures import Future
 from itertools import count
 from json import dumps, loads
+from threading import Thread
 from typing import Optional, Text
-from uuid import uuid4
 
 from websocket import WebSocket
 
@@ -10,6 +12,7 @@ class USDKConnection(object):
     def __init__(self, websocket):
         # type: (WebSocket) -> None
         self._websocket = websocket
+        self._keys = count(1)
 
     @classmethod
     def create(cls, port=2107):
@@ -25,7 +28,7 @@ class USDKConnection(object):
 
     def command(self, name, payload):
         # type: (Text, dict) -> dict
-        key = str(uuid4())
+        key = next(self._keys)
         self._websocket.send(dumps({"name": name, "key": key, "payload": payload}))
         response = loads(self._websocket.recv())
         assert response["name"] == name and response["key"] == key
@@ -38,3 +41,59 @@ class USDKConnection(object):
     def close(self):
         self._websocket.close()
         self._websocket = None
+
+
+class USDKSharedConnection(object):
+    def __init__(self, websocket):
+        # type: (WebSocket) -> None
+        self._websocket = websocket
+        self._keys = count(1)
+        self._response_futures = {}
+        self._receiver_thread = Thread(target=self._receiver_loop)
+        self._receiver_thread.start()
+
+    @classmethod
+    def create(cls, port=2107):
+        # type: (int) -> USDKConnection
+        websocket = WebSocket(enable_multithread=True)
+        websocket.connect("ws://localhost:{}/eyes".format(port))
+        websocket.settimeout(3 * 60)
+        return cls(websocket)
+
+    def notification(self, name, payload):
+        # type: (Text, dict) -> None
+        self._websocket.send(dumps({"name": name, "payload": payload}))
+
+    def command(self, name, payload):
+        # type: (Text, dict) -> dict
+        key = next(self._keys)
+        future = Future()
+        self._response_futures[key] = future
+        self._websocket.send(dumps({"name": name, "key": key, "payload": payload}))
+        return future.result()
+
+    def set_timeout(self, timeout):
+        # type: (Optional[float]) -> None
+        self._websocket.settimeout(timeout)
+
+    def close(self):
+        self._websocket.close()
+        self._websocket = None
+        self._receiver_thread.join()
+        self._receiver_thread = None
+
+    def _receiver_loop(self):
+        while True:
+            try:
+                resp = self._websocket.recv()
+                if not resp:
+                    for future in self._response_futures.values():
+                        future.set_exception(RuntimeError("USDK Connection closed"))
+                    break
+                response = loads(resp)
+                future = self._response_futures.pop(response["key"])
+                future.set_result(response)
+            except Exception:
+                for future in self._response_futures.values():
+                    future.set_exception_info(sys.exc_info()[1:])
+                break
--- eyes_selenium/applitools/selenium/server.py
+++ eyes_selenium/applitools/selenium/server.py
@@ -1,10 +1,10 @@
 from .command_executor import CommandExecutor
-from .connection import USDKConnection
+from .connection import USDKSharedConnection
 from .sdk_server import instance
 
 
 def connect():
     # type: () -> CommandExecutor
-    commands = CommandExecutor(USDKConnection.create(instance.port))
+    commands = CommandExecutor(USDKSharedConnection.create(instance.port))
     commands.make_sdk()
     return commands
