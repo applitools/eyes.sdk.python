2acfe1b8a0b0516bd92b657d15e931d89bcab31b
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-10-28 12:45:44 +0300

    test: back to python generated tests

--- coverage-tests/configuration/eg_configuration.js
+++ coverage-tests/configuration/eg_configuration.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "eyes_selenium_python",
     emitter: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/emitter.js",
     overrides: [
-        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/overrides.js",
+        "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/overrides.js",
         "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/eg.overrides.js",
     ],
     template: "https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs",
