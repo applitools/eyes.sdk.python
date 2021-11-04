f1367f387dc2db1e9d78533c79c4ca39b5b6e0e3
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-11-04 16:02:25 +0300

    test: js suite again

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'eyes_selenium_python',
     emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/emitter.js',
-    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/overrides.js',
+    overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/overrides.js',
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/python/template.hbs',
     ext: '.py',
     outPath: './test/coverage/generic',
