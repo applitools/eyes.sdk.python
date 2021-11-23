d84cd2a04187a8915f5204f192edf054a145a1bd
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-11-23 23:45:26 +0200

    fix: restore version hack to work on older setuptools

--- eyes_selenium/setup.py
+++ eyes_selenium/setup.py
@@ -1,3 +1,27 @@
+import codecs
+import re
+from os import path
+
 from setuptools import setup
 
-setup(package_data={"applitools.selenium": ["py.typed", "resources/*.js"]})
+here = path.abspath(path.dirname(__file__))
+
+
+def read(filename):
+    return codecs.open(path.join(here, filename), "r", "utf-8").read()
+
+
+def get_version(package_name):
+    version_file = read("applitools/{}/__version__.py".format(package_name))
+    try:
+        version = re.findall(r"^__version__ = \"([^']+)\"\r?$", version_file, re.M)[0]
+    except IndexError:
+        raise RuntimeError("Unable to determine version.")
+
+    return version
+
+
+setup(
+    version=get_version("selenium"),
+    package_data={"applitools.selenium": ["py.typed", "resources/*.js"]},
+)
