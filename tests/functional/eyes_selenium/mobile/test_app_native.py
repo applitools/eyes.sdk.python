772ab9263d5b8087742146df9f7834e39b37dac8
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-11-30 17:51:44 +0200

    test: disable and remove obsolete android tests

--- tests/functional/eyes_selenium/mobile/test_app_native.py
+++ tests/functional/eyes_selenium/mobile/test_app_native.py
@@ -47,6 +47,7 @@ def test_android_native(eyes, driver):
 @pytest.mark.platform("Android")
 @pytest.mark.capabilities(**ANDROID_CAPS)
 @pytest.mark.eyes_config(hide_scrollbars=False)
+@pytest.mark.skip("USDK Difference")
 def test_android_native_scale_app_native_feature(eyes, driver):
     eyes.configure.set_features(Feature.SCALE_MOBILE_APP)
     eyes.open(
--- tests/functional/eyes_selenium/mobile/test_app_native_sauce_labs.py
+++ tests/functional/eyes_selenium/mobile/test_app_native_sauce_labs.py
@@ -122,14 +122,3 @@ def test_iOS_native_region__sauce_labs(mobile_eyes):
         Region(10, 10, 20, 20), 3, 3, 20, 30
     )
     eyes.check("Contact list", settings)
-
-
-@pytest.mark.platform("Android")
-def test_android_native_sauce_labs_tracking_id_sent(mobile_eyes):
-    eyes, mobile_driver = mobile_eyes
-    eyes.open(mobile_driver, "AndroidNativeApp", "AndroidNativeApp trackingIdSent")
-    with patch("applitools.core.server_connector.ServerConnector.match_window") as smw:
-        eyes.check("Contact list", Target.window())
-        match_window_data = smw.call_args[0][1]  # type: MatchWindowData
-
-    assert match_window_data.options.source == "com.example.android.contactmanager"
