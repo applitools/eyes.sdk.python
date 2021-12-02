67883f6139a62d416122e65a30b2a3069ad16fb6
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-02 21:26:26 +0200

    test: skip usdk-specific test

--- tests/functional/eyes_selenium/visual_grid/test_bad_selectors.py
+++ tests/functional/eyes_selenium/visual_grid/test_bad_selectors.py
@@ -21,6 +21,7 @@ def test_check_region_with_bad_selector(driver, vg_runner):
         vg_runner.get_all_test_results()
 
 
+@pytest.mark.skip("USDK Difference")
 def test_check_region_with_bad_ignore_selector(driver, vg_runner):
     eyes = Eyes(vg_runner)
     driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage/")
