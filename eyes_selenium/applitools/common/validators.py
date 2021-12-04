9aae5c874ea905bb157e220288cd593d33dff68e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-04 14:25:30 +0200

    chore: fix python2

--- eyes_selenium/applitools/common/validators.py
+++ eyes_selenium/applitools/common/validators.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from appium.webdriver import WebElement as AppiumWebElement
 from selenium.webdriver.remote.webelement import WebElement as SeleniumWebElement
 from selenium.webdriver.support.event_firing_webdriver import EventFiringWebElement
@@ -8,6 +10,7 @@ def is_list_or_tuple(elm):
 
 
 def is_webelement(elm):
+
     return isinstance(
         elm,
         (SeleniumWebElement, AppiumWebElement, EventFiringWebElement),
