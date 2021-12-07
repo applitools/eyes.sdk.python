5d6f2d204ce499a543d308422edec7794a180242
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-07 10:19:42 +0200

    Delete test resources

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,9 @@
 from os import path
-from typing import TYPE_CHECKING, Dict, Text
+from typing import Dict, Text
 
 import requests
 
 from applitools.common import TestResults
-
-if TYPE_CHECKING:
-    from http.cookiejar import CookieJar
-
-    from applitools.common.utils.custom_types import AnyWebDriver
 
 TESTS_DIR = path.dirname(path.abspath(__file__))
 
@@ -35,30 +30,3 @@ def get_resource(name):
     pth = get_resource_path(name)
     with open(pth, "rb") as f:
         return f.read()
-
-
-def update_browser_cookies(cookies, required_domain, driver):
-    # type: (CookieJar, Text, AnyWebDriver) -> None
-    """Helps to reuse existing cookies with webdriver"""
-    from selenium.common.exceptions import (
-        InvalidCookieDomainException,
-        UnableToSetCookieException,
-    )
-
-    def dict_from_cookie(obj):
-        d = {n: v for n, v in vars(obj).items() if not n.startswith("_")}
-        d["secure"] = bool(d.get("secure", False))
-        return d
-
-    prev_url = None
-    for cookie in cookies:
-        if not cookie.domain.endswith(required_domain):
-            continue
-        next_url = "https://" + cookie.domain.lstrip(".")
-        if next_url != prev_url:
-            driver.get(next_url)
-            prev_url = next_url
-        try:
-            driver.add_cookie(dict_from_cookie(cookie))
-        except (UnableToSetCookieException, InvalidCookieDomainException):
-            print(cookie)
