ea31c2a5fe65fa89d163b6dafb4b021d94e7b878
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-07 11:05:12 +0200

    test: cleeanup tests

--- tests/functional/eyes_selenium/visual_grid/test_renderings.py
+++ tests/functional/eyes_selenium/visual_grid/test_renderings.py
@@ -1,5 +1,4 @@
 import time
-from collections import defaultdict
 
 import pytest
 
@@ -12,15 +11,11 @@ from applitools.selenium import (
     IosDeviceInfo,
     IosDeviceName,
     IosVersion,
-    MatchLevel,
     RectangleSize,
     ScreenOrientation,
-    StitchMode,
     Target,
     VisualGridRunner,
-    logger,
 )
-from tests.utils import get_session_results
 
 
 def test_mobile_only(driver, batch_info):
@@ -42,82 +37,6 @@ def test_mobile_only(driver, batch_info):
     eyes.check("Test Mobile Only", Target.window().fully())
     eyes.close()
     all_results = runner.get_all_test_results()
-
-
-@pytest.mark.skip
-def test_viewports_test(driver, batch_info):
-    runner = VisualGridRunner(30)
-    eyes = Eyes(runner)
-
-    sconf = Configuration()
-    sconf.batch = batch_info
-    sconf.test_name = "Viewport Size Test"
-    sconf.app_name = "Visual Grid Viewports Test"
-    sconf.hide_scrollbars = True
-    sconf.stitch_mode = StitchMode.CSS
-    sconf.force_full_page_screenshot = True
-    sconf.match_level = MatchLevel.STRICT
-
-    sconf.add_browser(800, 600, BrowserType.CHROME)
-    sconf.add_browser(700, 500, BrowserType.CHROME)
-    sconf.add_browser(1200, 800, BrowserType.CHROME)
-    sconf.add_browser(1600, 1200, BrowserType.CHROME)
-    sconf.add_browser(800, 600, BrowserType.FIREFOX)
-    sconf.add_browser(700, 500, BrowserType.FIREFOX)
-    sconf.add_browser(1200, 800, BrowserType.FIREFOX)
-    sconf.add_browser(1600, 1200, BrowserType.FIREFOX)
-    sconf.add_browser(800, 600, BrowserType.EDGE)
-    sconf.add_browser(700, 500, BrowserType.EDGE)
-    sconf.add_browser(1200, 800, BrowserType.EDGE)
-    # sconf.add_browser(1600, 1200, BrowserType.EDGE)
-    sconf.add_browser(800, 600, BrowserType.IE_11)
-    sconf.add_browser(700, 500, BrowserType.IE_11)
-    sconf.add_browser(1200, 800, BrowserType.IE_11)
-    # sconf.add_browser(1600, 1200, BrowserType.IE_11)
-    sconf.add_browser(800, 600, BrowserType.IE_10)
-    sconf.add_browser(700, 500, BrowserType.IE_10)
-    sconf.add_browser(1200, 800, BrowserType.IE_10)
-    # sconf.add_browser(1600, 1200, BrowserType.IE_10)
-    eyes.set_configuration(sconf)
-
-    eyes.open(driver)
-    driver.get("https://www.applitools.com")
-    eyes.check("Test Viewport", Target.window().fully())
-    eyes.close_async()
-
-    all_results = runner.get_all_test_results(False)
-    assert len(sconf.browsers_info) > len(BrowserType)
-    assert len(all_results) == len(sconf.browsers_info)
-
-    results = defaultdict(set)
-    for trc in all_results:
-        assert trc
-        session_results = None
-        try:
-            session_results = get_session_results(eyes.api_key, trc.test_results)
-        except Exception as e:
-            logger.exception(e)
-
-        if session_results is None:
-            logger.debug("Error: session_results is null for item {}".format(trc))
-            continue
-
-        env = session_results["env"]
-        browser = env["hostingAppInfo"]
-        if browser is None:
-            logger.debug("Error: HostingAppInfo (browser) is null. {}".format(trc))
-            continue
-
-        sizes_list = results[browser]
-        display_size = RectangleSize.from_(env["displaySize"])
-        if display_size in sizes_list:
-            assert (
-                False
-            ), "Browser {} viewport size {} already exists in results.".format(
-                browser, display_size
-            )
-        sizes_list.add(display_size)
-    assert len(results) == 5
 
 
 @pytest.mark.parametrize(
@@ -150,23 +69,6 @@ def test_special_rendering(url, test_name, batch_info, driver):
     eyes.check(test_name, Target.window().fully())
     eyes.close_async()
     all_results = runner.get_all_test_results()
-
-
-@pytest.mark.skip("Page is missing")
-def test_svg_parsing(driver, eyes, batch_info, vg_runner):
-    driver.get("https://danielschwartz85.github.io/static-test-page2/index.html")
-    eyes = Eyes(vg_runner)
-    eyes.set_configuration(
-        Configuration(
-            test_name="TestSvgParsing",
-            app_name="Visual Grid Render Test",
-            batch=batch_info,
-        ).add_browser(1200, 800, BrowserType.CHROME)
-    )
-    eyes.open(driver)
-    eyes.check_window()
-    eyes.close_async()
-    all_results = vg_runner.get_all_test_results()
 
 
 def test_css_relative_url_on_another_domain(driver, batch_info, vg_runner):
