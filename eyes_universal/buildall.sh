6f3875ef90cd76a0f6e01b70b7fa60c251090faa
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-07 16:35:06 +0200

    test: eyes_universal setup.py downloads target platform binary

--- eyes_universal/buildall.sh
+++
@@ -1,6 +0,0 @@
-python setup.py bdist_wheel --plat-name='macosx_10_7_x86_64'
-rm -r build eyes_universal.egg-info
-python setup.py bdist_wheel --plat-name='manylinux1_x86_64'
-rm -r build eyes_universal.egg-info
-python setup.py bdist_wheel --plat-name='win_amd64'
-rm -r build eyes_universal.egg-info
--- eyes_universal/setup.py
+++ eyes_universal/setup.py
@@ -1,22 +1,39 @@
+from distutils.util import get_platform
+from os import chmod, path
+
 from setuptools import setup
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from setuptools.command.build_py import build_py as _build_py
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
 
 
-class bdist_wheel(_bdist_wheel):
-    def run(self):
-        _, __, plat = _bdist_wheel.get_tag(self)
-        if "macos" in plat:
-            platform_bin[:] = ["bin/*-macos"]
-        elif "linux" in plat:
-            platform_bin[:] = ["bin/*-linux"]
-        elif "win" in plat:
-            platform_bin[:] = ["bin/*-win.exe"]
-        _bdist_wheel.run(self)
-
-
-platform_bin = ["bin/*"]
-
-setup(
-    package_data={"": platform_bin},
-    cmdclass={"bdist_wheel": bdist_wheel},
+download_dir = (
+    "https://github.com/applitools/eyes.sdk.javascript1/releases/download/"
+    "%40applitools%2Feyes-universal%40"
 )
+
+
+class build_py(_build_py):
+    def get_data_files(self):
+        platform = get_platform()
+        if "linux" in platform:
+            file_name = "eyes-universal-linux"
+        elif "macosx" in platform:
+            file_name = "eyes-universal-macos"
+        elif "win" in platform:
+            file_name = "eyes-universal-win.exe"
+        else:
+            raise RuntimeError("Unsupported platform", platform)
+        target = path.join("applitools", "eyes_universal", "bin", file_name)
+        url = "{}{}/{}".format(download_dir, self.distribution.get_version(), file_name)
+        if not path.isfile(target):
+            self.mkpath(path.dirname(target))
+            urlretrieve(url, target)
+            chmod(target, 0o755)
+        return _build_py.get_data_files(self)
+
+
+setup(cmdclass={"build_py": build_py})
