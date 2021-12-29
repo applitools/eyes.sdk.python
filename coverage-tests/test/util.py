5aa77468ecac13f9f713e91111c05f71cc8b00a0
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-29 23:24:56 +0200

    test: ssl verification in generated tests is back

--- coverage-tests/test/util.py
+++ coverage-tests/test/util.py
@@ -10,7 +10,6 @@ def get_test_info(api_key, results):
         "{}?format=json&AccessToken={}&apiKey={}".format(
             api_session_url, results.secret_token, api_key
         ),
-        verify=False,
     )
     r.raise_for_status()
     return r.json()
