269b0b12927539526209cc4797c5e852d3e9b3bf
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2021-12-30 16:16:33 +0200

    feat: nicier TargetPath

--- eyes_selenium/applitools/selenium/fluent/target_path.py
+++ eyes_selenium/applitools/selenium/fluent/target_path.py
@@ -1,96 +1,80 @@
 from typing import TYPE_CHECKING, overload
 
-import attr
 from selenium.webdriver.common.by import By
-from six import string_types
 
 if TYPE_CHECKING:
     from typing import Text
 
-    from applitools.common.utils.custom_types import BySelector
 
+class TargetPath(object):
+    @overload
+    def __init__(self, css_selector):
+        # type: (Text) -> None
+        pass
 
-@attr.s(repr=False)
-class ElementPath(object):
-    by = attr.ib(
-        type=string_types, repr=lambda v: "By.{}".format(v.upper().replace(" ", "_"))
-    )
-    selector = attr.ib(type=string_types)
-    shadow_path = attr.ib(type="ElementPath", default=None)
+    @overload
+    def __init__(self, by, selector):
+        # type: (Text, Text) -> None
+        pass
+
+    def __init__(self, css_selector_or_by, selector=None, shadow_path=None):
+        if selector is not None:
+            self.by = css_selector_or_by
+            self.selector = selector
+        else:
+            self.by = By.CSS_SELECTOR
+            self.selector = css_selector_or_by
+        self.shadow_path = shadow_path
 
     @overload
     def shadow(self, css_selector):
-        # type: (Text) -> ElementPath
+        # type: (Text) -> TargetPath
         pass
 
     @overload
-    def shadow(self, by_selector):
-        # type: (BySelector) -> ElementPath
+    def shadow(self, by, selector):
+        # type: (Text, Text) -> TargetPath
         pass
 
-    @overload
-    def shadow(self, elemement_path):
-        # type: (ElementPath) -> ElementPath
-        pass
-
-    def shadow(self, selector):
+    def shadow(self, css_selector_or_by, selector=None):
         if self.shadow_path:
-            self.shadow_path.shadow(selector)
-        elif isinstance(selector, string_types):
-            self.shadow_path = ElementPath(By.CSS_SELECTOR, selector)
-        elif isinstance(selector, list) and len(selector) == 2:
-            self.shadow_path = ElementPath(*selector)
-        elif isinstance(selector, ElementPath):
-            self.shadow_path = selector
+            self.shadow_path.shadow(css_selector_or_by, selector)
         else:
-            raise TypeError(
-                "Unsupported selector {} of type {}".format(selector, type(selector))
-            )
+            self.shadow_path = TargetPath(css_selector_or_by, selector)
         return self
 
+    def __eq__(self, that):
+        # type: (TargetPath) -> bool
+        return type(self) is type(that) and vars(self) == vars(that)
+
+    def __repr__1(self):
+        # type: () -> Text
+        if self.by == By.CSS_SELECTOR:
+            text = "{}({!r})".format(type(self).__name__, self.selector)
+        else:
+            text = "{}(By.{}, {!r})".format(
+                type(self).__name__, self.by.upper().replace(" ", "_"), self.selector
+            )
+        shadow = self.shadow_path
+        while shadow:
+            if shadow.by == By.CSS_SELECTOR:
+                text += ".shadow({!r})".format(shadow.selector)
+            else:
+                text += ".shadow(By.{}, {!r})".format(
+                    shadow.by.upper().replace(" ", "_"), shadow.selector
+                )
+            shadow = shadow.shadow_path
+        return text
+
     def __repr__(self):
-        by = "By.{}".format(self.by.upper().replace(" ", "_"))
-        shadow = ", " + repr(self.shadow_path) if self.shadow_path else ""
-        return "ElementPath({}, {!r}{})".format(by, self.selector, shadow)
-
-
-class TargetPath(object):
-    @staticmethod
-    def id(id_):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.ID, id_)
-
-    @staticmethod
-    def xpath(xpath):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.XPATH, xpath)
-
-    @staticmethod
-    def link_text(link_text):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.LINK_TEXT, link_text)
-
-    @staticmethod
-    def partial_link_text(partial_link_text):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.PARTIAL_LINK_TEXT, partial_link_text)
-
-    @staticmethod
-    def name(name):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.NAME, name)
-
-    @staticmethod
-    def tag_name(tag_name):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.TAG_NAME, tag_name)
-
-    @staticmethod
-    def class_name(class_name):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.CLASS_NAME, class_name)
-
-    @staticmethod
-    def css_selector(css_selector):
-        # type: (Text) -> ElementPath
-        return ElementPath(By.CSS_SELECTOR, css_selector)
+        # type: () -> Text
+        parts = []
+        call, path = type(self).__name__, self
+        while path is not None:
+            if path.by == By.CSS_SELECTOR:
+                parts.append("{}({!r})".format(call, path.selector))
+            else:
+                by = "By." + path.by.upper().replace(" ", "_")
+                parts.append("{}({}, {!r})".format(call, by, path.selector))
+            call, path = ".shadow", path.shadow_path
+        return "".join(parts)
--- tests/unit/eyes_selenium/test_target_path.py
+++ tests/unit/eyes_selenium/test_target_path.py
@@ -1,28 +1,26 @@
 from selenium.webdriver.common.by import By
 
-from applitools.selenium.fluent.target_path import ElementPath, TargetPath
+from applitools.selenium.fluent.target_path import TargetPath
 
 
 def test_element_path():
-    path = ElementPath(By.ID, "i")
+    path = TargetPath(By.ID, "i")
 
     assert path.by is By.ID
     assert path.selector == "i"
     assert path.shadow_path is None
 
 
-def test_element_path_with_init_shadow():
-    path = ElementPath(By.ID, "i", ElementPath(By.CSS_SELECTOR, "c"))
+def test_element_path_implicit():
+    path = TargetPath("c")
 
-    assert path.by is By.ID
-    assert path.selector == "i"
-    assert path.shadow_path.by is By.CSS_SELECTOR
-    assert path.shadow_path.selector == "c"
-    assert path.shadow_path.shadow_path is None
+    assert path.by is By.CSS_SELECTOR
+    assert path.selector == "c"
+    assert path.shadow_path is None
 
 
 def test_element_path_with_fluent_shadow():
-    path = ElementPath(By.ID, "i").shadow([By.CSS_SELECTOR, "c"])
+    path = TargetPath(By.ID, "i").shadow(By.CSS_SELECTOR, "c")
 
     assert path.by is By.ID
     assert path.selector == "i"
@@ -32,108 +30,44 @@ def test_element_path_with_fluent_shadow():
 
 
 def test_element_comparison():
-    assert ElementPath(By.ID, "i") == ElementPath(By.ID, "i")
-    assert ElementPath(By.ID, "i") != ElementPath(By.ID, "o")
-    assert ElementPath(By.ID, "i") != ElementPath(By.ID, "i", ElementPath(By.ID, "i"))
-
-
-def test_target_path_id():
-    path = TargetPath.id("a")
-
-    assert path.by is By.ID
-    assert path.selector == "a"
-    assert path.shadow_path is None
-
-
-def test_target_path_xpath():
-    path = TargetPath.xpath("x")
-
-    assert path.by is By.XPATH
-    assert path.selector == "x"
-    assert path.shadow_path is None
-
-
-def test_target_path_link_text():
-    path = TargetPath.link_text("l")
-
-    assert path.by is By.LINK_TEXT
-    assert path.selector == "l"
-    assert path.shadow_path is None
-
-
-def test_target_path_partial_link_text():
-    path = TargetPath.partial_link_text("p")
-
-    assert path.by is By.PARTIAL_LINK_TEXT
-    assert path.selector == "p"
-    assert path.shadow_path is None
-
-
-def test_target_path_name():
-    path = TargetPath.name("n")
-
-    assert path.by is By.NAME
-    assert path.selector == "n"
-    assert path.shadow_path is None
-
-
-def test_target_path_tag_name():
-    path = TargetPath.tag_name("t")
-
-    assert path.by is By.TAG_NAME
-    assert path.selector == "t"
-    assert path.shadow_path is None
-
-
-def test_target_path_class_name():
-    path = TargetPath.class_name("c")
-
-    assert path.by is By.CLASS_NAME
-    assert path.selector == "c"
-    assert path.shadow_path is None
-
-
-def test_target_path_css_selector():
-    path = TargetPath.css_selector("c")
-
-    assert path.by is By.CSS_SELECTOR
-    assert path.selector == "c"
-    assert path.shadow_path is None
+    assert TargetPath(By.ID, "i") == TargetPath(By.ID, "i")
+    assert TargetPath(By.ID, "i") != TargetPath(By.ID, "o")
+    assert TargetPath(By.ID, "i") != TargetPath(By.ID, "i").shadow(By.ID, "i")
 
 
 def test_target_path_id_with_implicit_css_shadow():
-    path = TargetPath.id("i").shadow("c")
+    path = TargetPath(By.ID, "i").shadow("c")
 
-    assert path == ElementPath(By.ID, "i", ElementPath(By.CSS_SELECTOR, "c"))
+    assert path == TargetPath(By.ID, "i", TargetPath(By.CSS_SELECTOR, "c"))  # noqa
 
 
-def test_target_path_id_with_two_level_implicit_css_shadows():
-    path = TargetPath.id("i").shadow("c").shadow("cc")
+def test_target_path_3_level_implicit_css_shadows():
+    path = TargetPath("c").shadow("cc").shadow("ccc")
 
-    assert path == ElementPath(
-        By.ID,
-        "i",
-        ElementPath(
+    assert path == TargetPath(  # noqa
+        By.CSS_SELECTOR,
+        "c",
+        TargetPath(  # noqa
             By.CSS_SELECTOR,
-            "c",
-            ElementPath(
+            "cc",
+            TargetPath(
                 By.CSS_SELECTOR,
-                "cc",
+                "ccc",
             ),
         ),
     )
 
 
 def test_target_path_id_with_two_level_shadows():
-    path = TargetPath.id("i").shadow([By.XPATH, "x"]).shadow([By.LINK_TEXT, "l"])
+    path = TargetPath(By.ID, "i").shadow(By.XPATH, "x").shadow(By.LINK_TEXT, "l")
 
-    assert path == ElementPath(
+    assert path == TargetPath(  # noqa
         By.ID,
         "i",
-        ElementPath(
+        TargetPath(  # noqa
             By.XPATH,
             "x",
-            ElementPath(
+            TargetPath(
                 By.LINK_TEXT,
                 "l",
             ),
@@ -141,19 +75,33 @@ def test_target_path_id_with_two_level_shadows():
     )
 
 
-def test_target_path_name_with_explicit_link_in_shadow():
-    path = TargetPath.name("n").shadow([By.LINK_TEXT, "l"])
+def test_target_path_css_with_explicit_link_in_shadow():
+    path = TargetPath("c").shadow(By.LINK_TEXT, "l")
 
-    assert path == ElementPath(By.NAME, "n", ElementPath(By.LINK_TEXT, "l"))
+    assert path == TargetPath(  # noqa
+        By.CSS_SELECTOR, "c", TargetPath(By.LINK_TEXT, "l")
+    )
 
 
-def test_target_path_repr():
-    path = TargetPath.name("n")
+def test_target_path_name_repr():
+    path = TargetPath(By.NAME, "n")
 
-    assert repr(path) == "ElementPath(By.NAME, 'n')"
+    assert repr(path) == "TargetPath(By.NAME, 'n')"
+
+
+def test_target_path_css_repr():
+    path = TargetPath("c")
+
+    assert repr(path) == "TargetPath('c')"
 
 
 def test_target_path_with_one_level_shadow_repr():
-    path = TargetPath.name("n").shadow("c")
+    path = TargetPath("c").shadow("cc")
 
-    assert repr(path) == "ElementPath(By.NAME, 'n', ElementPath(By.CSS_SELECTOR, 'c'))"
+    assert repr(path) == "TargetPath('c').shadow('cc')"
+
+
+def test_target_path_name_with_one_level_shadow_link_text_repr():
+    path = TargetPath(By.NAME, "n").shadow(By.LINK_TEXT, "l")
+
+    assert repr(path) == "TargetPath(By.NAME, 'n').shadow(By.LINK_TEXT, 'l')"
