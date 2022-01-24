525db8aa10da047ccb0aa96a54d7e09035c86a8a
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-01-24 19:58:06 +0200

    fix: correctly check WebDriver type

--- eyes_selenium/applitools/selenium/eyes.py
+++ eyes_selenium/applitools/selenium/eyes.py
@@ -132,7 +132,7 @@ class Eyes(object):
 
         results = self._commands.eyes_check(
             self._eyes_ref,
-            marshal_check_settings(isinstance(self._driver, WebDriver), check_settings),
+            marshal_check_settings(_is_selenium_driver(self.driver), check_settings),
             self._marshaled_configuration(),
         )
         if results:
@@ -449,7 +449,7 @@ class Eyes(object):
         return None
 
     def _marshaled_configuration(self):
-        return marshal_configuration(isinstance(self.driver, WebDriver), self.configure)
+        return marshal_configuration(_is_selenium_driver(self.driver), self.configure)
 
     def _close(self, raise_ex, wait_result):
         # type: (bool, bool) -> Optional[TestResults]
@@ -501,3 +501,15 @@ class Eyes(object):
             self._abort(self._runner.AUTO_CLOSE_MODE_SYNC)
         else:
             self._close(True, self._runner.AUTO_CLOSE_MODE_SYNC)
+
+
+def _is_selenium_driver(driver):
+    # type: (WebDriver) -> bool
+    try:
+        from appium.webdriver.webdriver import WebDriver as AppiumWebDriver
+
+        # appium.WebDriver is based on selenium.WebDriver so there is no sense to check
+        # for relationship with selenium.WebDriver. Have to eliminate appium instead
+        return not isinstance(driver, AppiumWebDriver)
+    except ImportError:
+        return True
