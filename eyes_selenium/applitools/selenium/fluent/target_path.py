8539f2ef92eebafa2b95fb0bace7de0487fa3480
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-02-14 19:07:24 +0200

    feat: TargetPath supports web element nodes

--- eyes_selenium/applitools/selenium/fluent/target_path.py
+++ eyes_selenium/applitools/selenium/fluent/target_path.py
@@ -1,61 +1,99 @@
 from typing import TYPE_CHECKING
 
 from selenium.webdriver.common.by import By
+from six import string_types
 
 if TYPE_CHECKING:
-    pass
+    from typing import Optional, Text, Union
+
+    from selenium.webdriver.remote.webelement import WebElement
 
 
-class Locator(object):
-    def __init__(self, parent, selector_or_by, selector=None):
-        self.parent = parent
+class PathNodeValue(object):
+    def __eq__(self, other):
+        # type: (PathNodeValue) -> bool
+        return type(self) is type(other) and vars(self) == vars(other)
+
+
+class ElementReference(PathNodeValue):
+    def __init__(self, element):
+        # type: (WebElement) -> None
+        self.element = element
+
+    def repr_args(self, _):
+        return "{!r}".format(self.element)
+
+
+class ElementSelector(PathNodeValue):
+    def __init__(self, selector_or_by, selector=None):
+        # type: (Text, Optional[Text]) -> None
         if selector is None:
-            self.by = By.CSS_SELECTOR if selector_or_by else None
+            self.by = By.CSS_SELECTOR
             self.selector = selector_or_by
         else:
             self.by = selector_or_by
             self.selector = selector
 
+    def repr_args(self, frame_call_args):
+        if self.by == By.CSS_SELECTOR and not frame_call_args:
+            return "{!r}".format(self.selector)
+        else:
+            by = "By." + self.by.upper().replace(" ", "_")
+            return "{}, {!r}".format(by, self.selector)
+
+
+class FrameSelector(PathNodeValue):
+    def __init__(self, number_or_id_or_name):
+        # type: (Union[int, Text]) -> None
+        self.number_or_id_or_name = number_or_id_or_name
+
+    def repr_args(self, _):
+        return "{!r}".format(self.number_or_id_or_name)
+
+
+class Locator(object):
+    def __init__(self, parent, value):
+        # type: (Optional[Locator], PathNodeValue) -> None
+        self.parent = parent
+        self.value = value
+
     def __eq__(self, other):
         return type(self) is type(other) and vars(self) == vars(other)
 
     def __repr__(self):
+        return self._repr(False)
+
+    def _repr(self, frame_call_args):
         parent = repr(self.parent) if self.parent else "TargetPath"
-        if self.by == By.CSS_SELECTOR:
-            return parent + ".{}({!r})".format(
-                self.FACTORY_METHOD, self.selector  # noqa
-            )
-        else:
-            return parent + ".{}({}, {!r})".format(
-                self.FACTORY_METHOD, _by_repr(self.by), self.selector  # noqa
-            )
+        args = self.value.repr_args(frame_call_args)
+        return parent + ".{}({})".format(self.FACTORY_METHOD, args)
 
 
 class TargetPath(object):
     @staticmethod
-    def region(selector_or_by, selector=None):
-        return RegionLocator(None, selector_or_by, selector)
+    def region(element_or_selector_or_by, selector=None):
+        return _region(None, element_or_selector_or_by, selector)
 
     @staticmethod
-    def frame(number_or_id_or_name_or_by, selector=None):
-        return FrameLocator(None, number_or_id_or_name_or_by, selector)
+    def frame(element_or_number_or_id_or_name_or_by, selector=None):
+        return _frame(None, element_or_number_or_id_or_name_or_by, selector)
 
     @staticmethod
-    def shadow(selector_or_by, selector=None):
-        return ShadowDomLocator(None, selector_or_by, selector)
+    def shadow(element_or_selector_or_by, selector=None):
+        return _shadow(None, element_or_selector_or_by, selector)
 
 
 class ShadowDomLocator(Locator):
     FACTORY_METHOD = "shadow"
 
-    def region(self, selector_or_by, selector=None):
-        return RegionLocator(self, selector_or_by, selector)
+    def region(self, element_or_selector_or_by, selector=None):
+        return _region(self, element_or_selector_or_by, selector)
 
-    def frame(self, number_or_id_or_name_or_by, selector=None):
-        return FrameLocator(self, number_or_id_or_name_or_by, selector)
+    def frame(self, element_or_number_or_id_or_name_or_by, selector=None):
+        return _frame(self, element_or_number_or_id_or_name_or_by, selector)
 
-    def shadow(self, selector_or_by, selector=None):
-        return ShadowDomLocator(self, selector_or_by, selector)
+    def shadow(self, element_or_selector_or_by, selector=None):
+        return _shadow(self, element_or_selector_or_by, selector)
 
 
 class RegionLocator(Locator):
@@ -65,36 +103,50 @@ class RegionLocator(Locator):
 class FrameLocator(Locator):
     FACTORY_METHOD = "frame"
 
-    def __init__(self, parent, number_or_id_or_name_or_by, selector=None):
-        if selector is None:
-            super(FrameLocator, self).__init__(parent, None, None)
-            self.number_or_id_or_name = number_or_id_or_name_or_by
-        else:
-            super(FrameLocator, self).__init__(
-                parent, number_or_id_or_name_or_by, selector
-            )
-            self.number_or_id_or_name = None
+    def region(self, element_or_selector_or_by, selector=None):
+        return _region(self, element_or_selector_or_by, selector)
+
+    def frame(self, element_or_number_or_id_or_name_or_by, selector=None):
+        return _frame(self, element_or_number_or_id_or_name_or_by, selector)
+
+    def shadow(self, element_or_selector_or_by, selector=None):
+        return _shadow(self, element_or_selector_or_by, selector)
 
     def __repr__(self):
-        parent = repr(self.parent) if self.parent else "TargetPath"
-        if self.number_or_id_or_name is not None:
-            return parent + ".{}({!r})".format(
-                self.FACTORY_METHOD, self.number_or_id_or_name
-            )
-        else:
-            return parent + ".{}({}, {!r})".format(
-                self.FACTORY_METHOD, _by_repr(self.by), self.selector
-            )
-
-    def region(self, selector_or_by, selector=None):
-        return RegionLocator(self, selector_or_by, selector)
-
-    def frame(self, number_or_id_or_name_or_by, selector=None):
-        return FrameLocator(self, number_or_id_or_name_or_by, selector)
-
-    def shadow(self, selector_or_by, selector=None):
-        return ShadowDomLocator(self, selector_or_by, selector)
+        return self._repr(True)
 
 
-def _by_repr(by):
-    return "By." + by.upper().replace(" ", "_")
+def _frame(parent, element_or_number_or_id_or_name_or_by, selector=None):
+    if selector is not None:
+        by = element_or_number_or_id_or_name_or_by
+        return FrameLocator(parent, ElementSelector(by, selector))
+    elif isinstance(element_or_number_or_id_or_name_or_by, (int, string_types)):
+        number_or_id_or_name = element_or_number_or_id_or_name_or_by
+        return FrameLocator(parent, FrameSelector(number_or_id_or_name))
+    else:
+        element = element_or_number_or_id_or_name_or_by
+        return FrameLocator(parent, ElementReference(element))
+
+
+def _shadow(parent, element_or_selector_or_by, selector=None):
+    if selector is not None:
+        by = element_or_selector_or_by
+        return ShadowDomLocator(parent, ElementSelector(by, selector))
+    elif isinstance(element_or_selector_or_by, string_types):
+        selector = element_or_selector_or_by
+        return ShadowDomLocator(parent, ElementSelector(By.CSS_SELECTOR, selector))
+    else:
+        element = element_or_selector_or_by
+        return ShadowDomLocator(parent, ElementReference(element))
+
+
+def _region(parent, element_or_selector_or_by, selector=None):
+    if selector is not None:
+        by = element_or_selector_or_by
+        return RegionLocator(parent, ElementSelector(by, selector))
+    elif isinstance(element_or_selector_or_by, string_types):
+        selector = element_or_selector_or_by
+        return RegionLocator(parent, ElementSelector(By.CSS_SELECTOR, selector))
+    else:
+        element = element_or_selector_or_by
+        return RegionLocator(parent, ElementReference(element))
--- tests/unit/eyes_selenium/test_target_path.py
+++ tests/unit/eyes_selenium/test_target_path.py
@@ -1,19 +1,27 @@
+from collections import namedtuple
+
 from selenium.webdriver.common.by import By
 
 from applitools.selenium.fluent.target_path import (
+    ElementReference,
+    ElementSelector,
     FrameLocator,
+    FrameSelector,
     RegionLocator,
     ShadowDomLocator,
     TargetPath,
 )
+
+DummyElement = namedtuple("DummyElement", "id")
 
 
 def test_target_path_region_by_css():
     path = TargetPath.region(".css")
 
     assert type(path) is RegionLocator
-    assert path.by is By.CSS_SELECTOR
-    assert path.selector == ".css"
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.CSS_SELECTOR
+    assert path.value.selector == ".css"
     assert path.parent is None
 
 
@@ -21,8 +29,9 @@ def test_target_path_region_by_xpath():
     path = TargetPath.region(By.XPATH, "//x")
 
     assert type(path) is RegionLocator
-    assert path.by is By.XPATH
-    assert path.selector == "//x"
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.XPATH
+    assert path.value.selector == "//x"
     assert path.parent is None
 
 
@@ -30,9 +39,8 @@ def test_target_path_frame_by_name_or_id():
     path = TargetPath.frame("name_or_id")
 
     assert type(path) is FrameLocator
-    assert path.by is None
-    assert path.selector is None
-    assert path.number_or_id_or_name == "name_or_id"
+    assert type(path.value) is FrameSelector
+    assert path.value.number_or_id_or_name == "name_or_id"
     assert path.parent is None
 
 
@@ -40,9 +48,8 @@ def test_target_path_frame_by_number():
     path = TargetPath.frame(1)
 
     assert type(path) is FrameLocator
-    assert path.by is None
-    assert path.selector is None
-    assert path.number_or_id_or_name == 1
+    assert type(path.value) is FrameSelector
+    assert path.value.number_or_id_or_name == 1
     assert path.parent is None
 
 
@@ -50,9 +57,9 @@ def test_target_path_frame_by_css_selector():
     path = TargetPath.frame(By.CSS_SELECTOR, ".css")
 
     assert type(path) is FrameLocator
-    assert path.by is By.CSS_SELECTOR
-    assert path.selector == ".css"
-    assert path.number_or_id_or_name is None
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.CSS_SELECTOR
+    assert path.value.selector == ".css"
     assert path.parent is None
 
 
@@ -60,8 +67,9 @@ def test_target_path_shadow_by_css():
     path = TargetPath.shadow(".css")
 
     assert type(path) is ShadowDomLocator
-    assert path.by is By.CSS_SELECTOR
-    assert path.selector == ".css"
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.CSS_SELECTOR
+    assert path.value.selector == ".css"
     assert path.parent is None
 
 
@@ -69,8 +77,36 @@ def test_target_path_shadow_by_xpath():
     path = TargetPath.shadow(By.XPATH, "//x")
 
     assert type(path) is ShadowDomLocator
-    assert path.by is By.XPATH
-    assert path.selector == "//x"
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.XPATH
+    assert path.value.selector == "//x"
+    assert path.parent is None
+
+
+def test_target_path_frame_element():
+    path = TargetPath.frame(DummyElement(1))
+
+    assert type(path) is FrameLocator
+    assert type(path.value) is ElementReference
+    assert path.value.element.id == 1
+    assert path.parent is None
+
+
+def test_target_path_shadow_element():
+    path = TargetPath.shadow(DummyElement(1))
+
+    assert type(path) is ShadowDomLocator
+    assert type(path.value) is ElementReference
+    assert path.value.element.id == 1
+    assert path.parent is None
+
+
+def test_target_path_region_element():
+    path = TargetPath.region(DummyElement(1))
+
+    assert type(path) is RegionLocator
+    assert type(path.value) is ElementReference
+    assert path.value.element.id == 1
     assert path.parent is None
 
 
@@ -78,10 +114,13 @@ def test_target_region_within_frame():
     path = TargetPath.frame(1).region(".css")
 
     assert type(path) is RegionLocator
-    assert path.by is By.CSS_SELECTOR
-    assert path.selector == ".css"
+    assert type(path.value) is ElementSelector
+    assert path.value.by is By.CSS_SELECTOR
+    assert path.value.selector == ".css"
     assert type(path.parent) is FrameLocator
-    assert path.parent.number_or_id_or_name == 1
+    assert type(path.parent.value) is FrameSelector
+    assert path.parent.value.number_or_id_or_name == 1
+    assert path.parent.parent is None
 
 
 def test_target_path_region_css_repr():
@@ -96,6 +135,12 @@ def test_target_path_region_xpath_repr():
     assert repr(path) == "TargetPath.region(By.XPATH, '//x')"
 
 
+def test_target_path_region_element_repr():
+    path = TargetPath.region(DummyElement(1))
+
+    assert repr(path) == "TargetPath.region(DummyElement(id=1))"
+
+
 def test_target_path_shadow_css_repr():
     path = TargetPath.shadow(".css")
 
@@ -106,6 +151,12 @@ def test_target_path_shadow_xpath_repr():
     path = TargetPath.shadow(By.XPATH, "//x")
 
     assert repr(path) == "TargetPath.shadow(By.XPATH, '//x')"
+
+
+def test_target_path_shadow_element_repr():
+    path = TargetPath.shadow(DummyElement(1))
+
+    assert repr(path) == "TargetPath.shadow(DummyElement(id=1))"
 
 
 def test_target_path_frame_number_repr():
@@ -126,6 +177,12 @@ def test_target_path_frame_css_repr():
     assert repr(path) == "TargetPath.frame(By.CSS_SELECTOR, '.css')"
 
 
+def test_target_path_frame_element_repr():
+    path = TargetPath.frame(DummyElement(1))
+
+    assert repr(path) == "TargetPath.frame(DummyElement(id=1))"
+
+
 def test_target_path_shadow_css_region_css_repr():
     path = TargetPath.shadow("#shadow").region(".region")
 
@@ -136,6 +193,12 @@ def test_target_path_frame_shadow_region_repr():
     path = TargetPath.frame(1).shadow("#shadow").region(".region")
 
     assert repr(path) == "TargetPath.frame(1).shadow('#shadow').region('.region')"
+
+
+def test_target_path_frame_element_shadow_region_repr():
+    path = TargetPath.frame(DummyElement(2)).shadow("#").region(".")
+
+    assert repr(path) == "TargetPath.frame(DummyElement(id=2)).shadow('#').region('.')"
 
 
 def test_target_path_region_eq():
