a222c9ba86bf44b2bca12e7a8fb8db124c0bc176
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-05-02 23:49:27 +0200

    get back to main test branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,12 +1,12 @@
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/fix_wait_before_capture_test/python/emitter.js',
+    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/emitter.js',
     overrides: [
         'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
         'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/overrides.js',
     ],
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/template.hbs',
-    tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/fix_wait_before_capture_test/coverage-tests.js',
+    tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
     ext: '.py',
     outPath: './test/coverage/generic',
 }
