b90cf0e482aa92b5f934bc632fdfc3517fdb7f8b
Serhii Khalymon <sergiykhalimon@gmail.com>
2022-05-02 15:53:25 +0300

    test: remove unused tests

--- tests/functional/eyes_selenium/visual_grid/test_bad_selectors.py
+++ tests/functional/eyes_selenium/visual_grid/test_bad_selectors.py
@@ -5,7 +5,7 @@ from applitools.common import RectangleSize
 from applitools.selenium import Eyes, Target
 
 
-@pytest.mark.skip("USDK Difference, bad region is ignored")
+@pytest.mark.skip("USDK Difference, Element not exists is ignored")
 def test_check_region_with_bad_selector(driver, vg_runner):
     eyes = Eyes(vg_runner)
     driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage/")
@@ -17,32 +17,9 @@ def test_check_region_with_bad_selector(driver, vg_runner):
     )
     with pytest.raises(NoSuchElementException):
         eyes.check_region("#element_that_does_not_exist")
-        eyes.close_async()
-        vg_runner.get_all_test_results()
 
 
-@pytest.mark.skip("USDK Difference, smaller screenshot size viewport instead of fully")
-def test_check_region_with_bad_ignore_selector(driver, vg_runner):
-    eyes = Eyes(vg_runner)
-    driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage/")
-    eyes.open(
-        driver,
-        "Applitools Eyes Python SDK",
-        "TestCheckRegionWithBadIgnoreSelector_VG",
-        RectangleSize(800, 600),
-    )
-    eyes.check(
-        "",
-        Target.window()
-        .ignore("body>p:nth-of-type(" "14)")
-        .before_render_screenshot_hook(
-            "var p = document.querySelector('body>p:nth-of-type(14)'); p.parentNode.removeChild(p);"
-        ),
-    )
-    eyes.close()
-    vg_runner.get_all_test_results()
-
-
+@pytest.mark.skip("USDK Difference, Element not exists is ignored")
 def test_check_region_with_bad_selector_before_valid_check(driver, vg_runner):
     eyes = Eyes(vg_runner)
     driver.get("https://applitools.github.io/demo/TestPages/VisualGridTestPage/")
@@ -54,8 +31,3 @@ def test_check_region_with_bad_selector_before_valid_check(driver, vg_runner):
     )
     with pytest.raises(NoSuchElementException):
         eyes.check_region("#element_that_does_not_exist")
-        driver.find_element_by_id("centred").click()
-        eyes.check_region("#modal-content")
-
-        eyes.close_async()
-        vg_runner.get_all_test_results()
