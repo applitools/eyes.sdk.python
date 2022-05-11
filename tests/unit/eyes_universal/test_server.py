e76ab6e7325ac2db2d6fb6a8922929e080de6fc8
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-05-11 17:43:57 +0200

    Use fork mode option when starting eyes-server (#438)
    * test: update eyes-universal to 2.5.5
    * feat: use simplified fork mode when starting server

--- tests/unit/eyes_universal/test_server.py
+++ tests/unit/eyes_universal/test_server.py
@@ -7,135 +7,52 @@ from applitools.eyes_universal.server import SDKServer, executable_path
 
 
 @pytest.fixture
-def popen(monkeypatch):
-    popen_mock = Mock()
-    monkeypatch.setattr("applitools.eyes_universal.server.Popen", popen_mock)
-    return popen_mock
+def check_output(monkeypatch):
+    mock = Mock(return_value=b"123\n")
+    monkeypatch.setattr("applitools.eyes_universal.server.check_output", mock)
+    return mock
 
 
-@pytest.fixture
-def tempfile(monkeypatch):
-    tempfile_mock = Mock()
-    tempfile_mock.return_value.readline.side_effect = [b"123\n"]
-    tempfile_mock.return_value.name = "temp_file_name"
-    monkeypatch.setattr(
-        "applitools.eyes_universal.server.NamedTemporaryFile", tempfile_mock
-    )
-    return tempfile_mock
-
-
-@pytest.fixture
-def close_fds_kw():
-    if sys.version_info < (3,) and sys.platform != "win32":
-        return {"close_fds": True}
-    else:
-        return {}
-
-
-def test_sdk_server_default_args(popen, tempfile, close_fds_kw):
+def test_sdk_server_default_args(check_output):
     server = SDKServer()
-    server.close()
 
     assert server.port == 123
-    assert server.log_file_name == "temp_file_name"
-    assert popen.mock_calls == [
-        call([executable_path], stdout=tempfile.return_value, stderr=-2, **close_fds_kw)
-    ]
-    assert tempfile.mock_calls == [
-        call("w+b"),
-        call().seek(0),
-        call().readline(),
-        call().close(),
+    assert check_output.mock_calls == [
+        call([executable_path, "--fork"], universal_newlines=True)
     ]
 
 
-def test_sdk_server_file_not_read_immediately(popen, tempfile):
-    tempfile.return_value.readline.side_effect = [b"", b"123\n"]
-    server = SDKServer()
-    server.close()
-
-    assert tempfile.mock_calls == [
-        call("w+b"),
-        call().seek(0),
-        call().readline(),
-        call().seek(0),
-        call().readline(),
-        call().close(),
-    ]
-
-
-def test_sdk_server_with_port(popen, tempfile, close_fds_kw):
+def test_sdk_server_with_port(check_output):
     server = SDKServer(port=345)
-    server.close()
 
     assert server.port == 123  # mock return this
-    assert server.log_file_name == "temp_file_name"
-    assert popen.mock_calls == [
+    assert check_output.mock_calls == [
         call(
-            [executable_path, "--port", 345],
-            stdout=tempfile.return_value,
-            stderr=-2,
-            **close_fds_kw
+            [executable_path, "--port", 345, "--fork"],
+            universal_newlines=True,
         )
     ]
-    assert tempfile.mock_calls == [
-        call("w+b"),
-        call().seek(0),
-        call().readline(),
-        call().close(),
-    ]
 
 
-def test_sdk_server_no_singleton(popen, tempfile, close_fds_kw):
-    server = SDKServer(singleton=False)
-    server.close()
-
-    assert server.port == 123
-    assert server.log_file_name == "temp_file_name"
-    assert popen.mock_calls == [
-        call(
-            [executable_path, "--no-singleton"],
-            stdout=tempfile.return_value,
-            stderr=-2,
-            **close_fds_kw
-        ),
-        call().terminate(),
-    ]
-    assert tempfile.mock_calls == [
-        call("w+b"),
-        call().seek(0),
-        call().readline(),
-        call().close(),
-    ]
-
-
-def test_sdk_server_lazy(popen, tempfile, close_fds_kw):
+def test_sdk_server_lazy(check_output):
     server = SDKServer(lazy=True)
-    server.close()
 
     assert server.port == 123
-    assert server.log_file_name == "temp_file_name"
-    assert popen.mock_calls == [
+    assert check_output.mock_calls == [
         call(
-            [executable_path, "--lazy"],
-            stdout=tempfile.return_value,
-            stderr=-2,
-            **close_fds_kw
-        ),
+            [executable_path, "--lazy", "--fork"],
+            universal_newlines=True,
+        )
     ]
 
 
-def test_sdk_server_idle_timeout(popen, tempfile, close_fds_kw):
-    server = SDKServer(idle_timeout=True)
-    server.close()
+def test_sdk_server_idle_timeout(check_output):
+    server = SDKServer(idle_timeout=5)
 
     assert server.port == 123
-    assert server.log_file_name == "temp_file_name"
-    assert popen.mock_calls == [
+    assert check_output.mock_calls == [
         call(
-            [executable_path, "--idle-timeout", 1],
-            stdout=tempfile.return_value,
-            stderr=-2,
-            **close_fds_kw
-        ),
+            [executable_path, "--idle-timeout", 5, "--fork"],
+            universal_newlines=True,
+        )
     ]
