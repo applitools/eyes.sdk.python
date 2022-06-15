ffac96c985aa65dd8e6e794b83c6863cb75e624b
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-15 16:50:59 +0300

    feat: switch generator to implement_lazy_load_emitter test branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/emitter.js',
+    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/implement_lazy_load_emitter/python/emitter.js',
     overrides: [
         'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
-        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/overrides.js',
+        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/implement_lazy_load_emitter/python/overrides.js',
     ],
     template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/python/template.hbs',
     tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/coverage-tests.js',
