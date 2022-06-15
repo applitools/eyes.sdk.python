6659f1f0d8ab44b50aa5d22f33456f33972860ce
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-15 12:03:21 +0300

    fix: use appium objects for driver configuration

--- coverage-tests/test/devices.py
+++ coverage-tests/test/devices.py
@@ -2,6 +2,8 @@ import os
 
 import pytest
 from appium import webdriver as appium_webdriver
+from appium.options.android import UiAutomator2Options
+from appium.options.ios import XCUITestOptions
 
 from . import sauce
 
@@ -24,84 +26,88 @@ def browser_name():
 @sauce.vm
 @pytest.fixture(scope="function")
 def pixel_3_xl(app, sauce_url, browser_name, orientation, name_of_test):
-    desired_caps = {
-        "deviceName": "Google Pixel 3 XL GoogleAPI Emulator",
-        "platformVersion": "10.0",
-        "platformName": "Android",
-        "clearSystemFiles": True,
-        "noReset": True,
-        "automationName": "UiAutomator2",
-        "name": name_of_test,
-        "sauce:options": {"deviceOrientation": orientation.upper()},
+    sauce_options = {
         "appiumVersion": "1.20.2",
+        "deviceOrientation": orientation.upper(),
+        "name": name_of_test,
+        "noReset": True,
     }
-    return appium(desired_caps, sauce_url, app=app, browser_name=browser_name)
+    options = (
+        UiAutomator2Options()
+        .set_capability("clearSystemFiles", True)
+        .set_capability("deviceName", "Google Pixel 3 XL GoogleAPI Emulator")
+        .set_capability("platformVersion", "10.0")
+        .set_capability("sauce:options", sauce_options)
+    )
+    return _appium(options, sauce_url, app=app, browser_name=browser_name)
 
 
 @sauce.vm
 @pytest.fixture(scope="function")
 def pixel_3a_xl(app, sauce_url, browser_name, orientation, name_of_test):
-    desired_caps = {
-        "deviceName": "Google Pixel 3a XL GoogleAPI Emulator",
-        "platformVersion": "10.0",
-        "platformName": "Android",
-        "clearSystemFiles": True,
-        "noReset": True,
-        "automationName": "UiAutomator2",
-        "name": name_of_test,
-        "sauce:options": {"deviceOrientation": orientation.upper()},
+    sauce_options = {
         "appiumVersion": "1.20.2",
+        "deviceOrientation": orientation.upper(),
+        "name": name_of_test,
+        "noReset": True,
     }
-    return appium(desired_caps, sauce_url, app=app, browser_name=browser_name)
+    options = (
+        UiAutomator2Options()
+        .set_capability("clearSystemFiles", True)
+        .set_capability("deviceName", "Google Pixel 3a XL GoogleAPI Emulator")
+        .set_capability("platformVersion", "10.0")
+        .set_capability("sauce:options", sauce_options)
+    )
+    return _appium(options, sauce_url, app=app, browser_name=browser_name)
 
 
 @sauce.vm
 @pytest.fixture(scope="function")
 def samsung_galaxy_s8(app, sauce_url, browser_name, orientation, name_of_test):
-    desired_caps = {
-        "deviceName": "Samsung Galaxy S8 FHD GoogleAPI Emulator",
-        "platformVersion": "7.0",
-        "platformName": "Android",
-        "clearSystemFiles": True,
-        "noReset": True,
-        "automationName": "UiAutomator2",
-        "name": name_of_test,
-        "sauce:options": {"deviceOrientation": orientation.upper()},
+    sauce_options = {
         "appiumVersion": "1.19.2",
+        "deviceOrientation": orientation.upper(),
+        "name": name_of_test,
+        "noReset": True,
     }
-    return appium(desired_caps, sauce_url, app=app, browser_name=browser_name)
+    options = (
+        UiAutomator2Options()
+        .set_capability("clearSystemFiles", True)
+        .set_capability("deviceName", "Samsung Galaxy S8 FHD GoogleAPI Emulator")
+        .set_capability("platformVersion", "7.0")
+        .set_capability("sauce:options", sauce_options)
+    )
+    return _appium(options, sauce_url, app=app, browser_name=browser_name)
 
 
 @sauce.mac_vm
 @pytest.fixture(scope="function")
 def iphone_xs(app, sauce_url, browser_name, orientation, name_of_test):
-    desired_caps = {
-        "deviceName": "iPhone XS Simulator",
-        "platformVersion": "13.0",
-        "platformName": "iOS",
-        "clearSystemFiles": True,
-        "noReset": True,
-        "automationName": "XCUITest",
-        "name": name_of_test,
-        "deviceOrientation": orientation,
+    sauce_options = {
         "appiumVersion": "1.19.2",
+        "deviceOrientation": orientation.upper(),
+        "name": name_of_test,
+        "noReset": True,
     }
-    return appium(desired_caps, sauce_url, app=app, browser_name=browser_name)
+    options = (
+        XCUITestOptions()
+        .set_capability("clearSystemFiles", True)
+        .set_capability("deviceName", "iPhone XS Simulator")
+        .set_capability("platformVersion", "13.0")
+        .set_capability("sauce:options", sauce_options)
+    )
+    return _appium(options, sauce_url, app=app, browser_name=browser_name)
 
 
-def appium(desired_caps, sauce_url, app="", browser_name=""):
+def _appium(options, sauce_url, app="", browser_name=""):
     if app and browser_name:
         raise Exception("Appium drivers shouldn't contain both app and browserName")
     if not app and not browser_name:
         raise Exception("Appium drivers should have app or browserName")
     if app:
-        desired_caps["app"] = app
-        desired_caps["NATIVE_APP"] = True
-        desired_caps["browserName"] = ""
+        options = options.set_capability("app", app).set_capability("NATIVE_APP", True)
     if browser_name:
-        desired_caps["browserName"] = browser_name
+        options = options.set_capability("browserName", browser_name)
 
     selenium_url = os.getenv("SELENIUM_SERVER_URL", sauce_url)
-    return appium_webdriver.Remote(
-        command_executor=selenium_url, desired_capabilities=desired_caps
-    )
+    return appium_webdriver.Remote(command_executor=selenium_url, options=options)
