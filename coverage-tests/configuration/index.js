c24d078609e7cae0dfd86e5d817e96b8ace1c240
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-17 20:28:47 +0300

    test: update test ref

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,12 +1,13 @@
+let ref = 'c80c7df'
 module.exports = {
     name: 'eyes_selenium_python',
-    emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/d5e3833f2fac/python/emitter.js',
+    emitter: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/emitter.js`,
     overrides: [
-        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/d5e3833f2fac/js/overrides.js',
-        'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/d5e3833f2fac/python/overrides.js',
+        `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/js/overrides.js`,
+        `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/overrides.js`,
     ],
-    template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/d5e3833f2fac/python/template.hbs',
-    tests: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/d5e3833f2fac/coverage-tests.js',
+    template: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/template.hbs`,
+    tests: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/coverage-tests.js`,
     ext: '.py',
     outPath: './test/coverage/generic',
 }
