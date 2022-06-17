e753f18c10361c2464becf10264a7a05f8975cf4
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-17 22:50:36 +0300

    test: update ref

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,4 +1,4 @@
-let ref = 'e25aea91'
+let ref = '6f22b01f'
 module.exports = {
     name: 'eyes_selenium_python',
     emitter: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/emitter.js`,
