9858ac2ebf601963faac94267e7673bb2b0c9402
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-18 10:29:11 +0300

    test: cleanup global conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,5 @@
 import os
+import uuid
 
-import mock
-import pytest
-
-
-def pytest_generate_tests(metafunc):
-    import uuid
-
-    # setup environment variables once per test run if not settled up
-    # needed for multi thread run
-
-    os.environ["APPLITOOLS_BATCH_ID"] = os.getenv(
-        "APPLITOOLS_BATCH_ID", str(uuid.uuid4())
-    )
-
-
-@pytest.fixture
-def driver_mock():
-    from selenium.webdriver.remote.webdriver import WebDriver
-
-    return mock.Mock(WebDriver)
+# Generate APPLITOOLS_BATCH_ID for xdist run in case it was not provided externally
+os.environ["APPLITOOLS_BATCH_ID"] = os.getenv("APPLITOOLS_BATCH_ID", str(uuid.uuid4()))
