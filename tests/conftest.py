86269ffdb11fb11407000112fd877256b6ff9e3e
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-18 11:55:15 +0300

    test: fix imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,5 +14,4 @@ os.environ["APPLITOOLS_BATCH_ID"] = os.getenv("APPLITOOLS_BATCH_ID", str(uuid.uu
 # produce these warnings.
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", "invalid escape", category=DeprecationWarning)
-    import kitchen.text.converters
     import selenium.webdriver.remote.webdriver
