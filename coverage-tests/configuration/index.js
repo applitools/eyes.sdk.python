92df4a45b67fd3ba974696a833b2a93639fe87d3
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-06-28 15:47:06 +0300

    test: use pytho_emitters_padded_regions branch

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,10 +1,10 @@
-let ref = "b1e7d6b3";
+let ref = "pytho_emitters_padded_regions";
 module.exports = {
     name: 'eyes_selenium_python',
     emitter: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/emitter.js`,
     overrides: [
         `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/js/overrides.js`,
-        `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/overrides.js`,
+        `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/b1e7d6b3/python/overrides.js`,
     ],
     template: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/python/template.hbs`,
     tests: `https://raw.githubusercontent.com/applitools/sdk.coverage.tests/${ref}/coverage-tests.js`,
